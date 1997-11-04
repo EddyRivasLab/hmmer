52b0b596340d410b4cb9c39f177e3c5742362b0b
Sean Eddy <sean@eddylab.org>
1997-11-04 17:45:26 +0000

    updated to Jan 98: target for hmmer2 release
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/trunk/hmmer@24 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- src/version.h
+++ src/version.h
@@ -10,16 +10,17 @@
 
 /* version.h
  * Version and copyright stamp information for the package.
+ * RCS $Header$
  */
 
 
 #define RELEASE     "2.0"
-#define RELEASEDATE "June 1997"
+#define RELEASEDATE "Jan 1998"
 
 #ifndef LINTING			/* compiled-in stamp for all binaries  */
 static char *copyright = "\
-   HMMER -- Biological sequence analysis with profile-HMMs.\n\
-   Copyright (C) 1992-1997 Sean R. Eddy.\n\
+   HMMER -- Biological sequence analysis with profile HMMs.\n\
+   Copyright (C) 1992-1998 Sean R. Eddy.\n\
 \n\
    HMMER is distributed under the terms of the GNU General Public\n\
    License. See the files COPYING and GNULICENSE for details.";
