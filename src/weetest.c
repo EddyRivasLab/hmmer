8611a1063ea9b35a7964504979e08c7ce4e2ac76
Sean Eddy <sean@eddylab.org>
1997-12-21 17:57:38 +0000

    Used to fit Gaussian length distribution to Swissprot 34
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/trunk/hmmer@84 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- src/weetest.c
+++ src/weetest.c
@@ -1,9 +1,18 @@
-/* Compile with:
+/* This is a throwaway wrapper program for doing quick
+ * and dirty tests on sequence databases. Archives of past
+ * versions are kept and logged in RCS.
+ * RCS $Id$
  * 
- cc -g -o weetest -I ~/lib/squid.linux -L/nfs/wol2/people/eddy/lib/squid.linux weetest.c alphabet.o camJul97.o core_algorithms.o histogram.o hmmio.o mathsupport.o masks.o misc.o modelmakers.o debug.o prior.o trace.o plan7.o states.o tophits.o -lsquid-debug -lm
+ * Compile with:
  * 
+cc -g -o weetest -I ~/lib/squid.linux -L/nfs/wol2/people/eddy/lib/squid.linux weetest.c alphabet.o camJul97.o core_algorithms.o histogram.o hmmio.o mathsupport.o masks.o misc.o modelmakers.o debug.o prior.o trace.o plan7.o states.o tophits.o -lsquid-debug -lm
+ *
+ * or, for optimized version:
+cc -O2 -o weetest -I ~/lib/squid.linux -L/nfs/wol2/people/eddy/lib/squid.linux weetest.c alphabet.o camJul97.o core_algorithms.o histogram.o hmmio.o mathsupport.o masks.o misc.o modelmakers.o debug.o prior.o trace.o plan7.o states.o tophits.o -lsquid -lm
  */
 
+/* This test looks at histogram of protein lengths in Swissprot
+ */
 #include <stdio.h>
 
 #include "structs.h"
@@ -14,51 +23,33 @@
 int
 main(int argc, char **argv)
 {
-  char *file;
-  char *seq;
-  char *dsq;
-  int   i,x;
+  char   *file;
+  char   *seq;
+  char   *dsq;
   int     format;
   SQFILE *sqfp;
   SQINFO  sqinfo;
-  float xp[20] = { 0.0681, 0.0120, 0.0623, 0.0651, 0.0313, 0.0902, 0.0241, 0.0371, 0.0687, 0.0676,
-		   0.0143, 0.0548, 0.0647, 0.0415, 0.0551, 0.0926, 0.0623, 0.0505, 0.0102, 0.0269
-                 };
-  int sc[23];
-  int score;
-  float perpos;
-  int  correction;
+  int     i,x;
+
+  struct histogram_s *h;
 
   file = argv[1];
-  SetAlphabet(hmmAMINO);
+  if (! SeqfileFormat(file, &format, "BLASTDB")) 
+    Die("SeqfileFormat()");
+  if ((sqfp = SeqfileOpen(file, format, "BLASTDB")) == NULL) 
+    Die("SeqfileOpen()");
 
-  for (x = 0; x < 20; x++)  sc[x] = Prob2Score(xp[x], aafq[x]);
-  sc[20] = DegenerateSymbolScore(xp, aafq, 20);
-  sc[21] = DegenerateSymbolScore(xp, aafq, 21);
-  sc[22] = DegenerateSymbolScore(xp, aafq, 22);
-
-  if (! SeqfileFormat(file, &format, NULL))             Die("SeqfileFormat()");
-  if ((sqfp = SeqfileOpen(file, format, NULL)) == NULL) Die("SeqfileOpen()");
-
+  h = AllocHistogram(0, 10000, 1000);
   while (ReadSeq(sqfp, format, &seq, &sqinfo)) 
-    {
-      dsq = DigitizeSequence(seq, sqinfo.len);
+    AddToHistogram(h, (float) sqinfo.len);
 
-      score = 0;
-      for (i = 1; i <= sqinfo.len; i++) 
-	score += sc[(int) dsq[i]];
+  GaussianFitHistogram(h, 999999.);
+  PrintASCIIHistogram(stdout, h);
 
-      correction = ILogsum(0, score);
+  printf("mean = %f\n", h->param[GAUSS_MEAN]);
+  printf("sd   = %f\n", h->param[GAUSS_SD]);
 
-      perpos = Scorify(score);
-      perpos /= (float) sqinfo.len;
-      printf("%-20s  %.1f\t%.2f\t%.2f\n", sqinfo.name, Scorify(score), perpos,
-	     Scorify(correction));
-
-      free(dsq);
-      free(seq);
-    }
   SeqfileClose(sqfp);
-
+  
   return EXIT_SUCCESS;
 }
