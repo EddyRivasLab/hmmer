492ca3322cce1ed47fcfa1489412377cb0c3e5e4
Sean Eddy <sean@eddylab.org>
1997-12-21 17:55:15 +0000

    new Swissprot-calibrated length distribution gaussian
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/trunk/hmmer@79 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- src/hmmcalibrate.c
+++ src/hmmcalibrate.c
@@ -1,6 +1,7 @@
 /************************************************************
  * HMMER - Biological sequence analysis with profile-HMMs
- * Copyright (C) 1992-1997 Sean R. Eddy
+ * Copyright (C) 1992-1998,
+ * Sean R. Eddy and Washington University School of Medicine
  *
  *   This source code is distributed under the terms of the
  *   GNU General Public License. See the files COPYING and
@@ -20,6 +21,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <math.h>
 #include <time.h>
 #include <limits.h>
@@ -43,16 +45,20 @@ static char usage[] = "\
 Usage: hmmcalibrate [-options] <hmmfile>\n\
 Option        : Default : Description\n\
   -h          :       - : print short usage and version info, then exit\n\
-  -l <x>      :     2.0 : set random sequence length multiplier to <x>\n\
+  -l <n>      :       - : fix random sequence length at <n>\n\
+  -m <x>      :     350 : set random seq length mean at <x>\n\
   -n <n>      :   10000 : set number of sampled seqs to <n>\n\
+  -s <x>      :     350 : set random seq length std. dev to <x>\n\
 \n\
-  --histfile <f>        : save histograms to file <f>\n\
+  --histfile <f> :    - : save histograms to file <f>\n\
 ";
 
 static struct opt_s OPTIONS[] = {
-   { "-h", TRUE, sqdARG_NONE   },
-   { "-l", TRUE, sqdARG_FLOAT  },
-   { "-n", TRUE, sqdARG_INT    },
+   { "-h", TRUE, sqdARG_NONE  },
+   { "-l", TRUE, sqdARG_INT   },
+   { "-m", TRUE, sqdARG_FLOAT },
+   { "-n", TRUE, sqdARG_INT   },
+   { "-s", TRUE, sqdARG_FLOAT },   
    { "--histfile", FALSE, sqdARG_STRING },
 };
 #define NOPTIONS (sizeof(OPTIONS) / sizeof(struct opt_s))
@@ -75,10 +81,13 @@ main(int argc, char **argv)
   float   score;		/* score of an alignment           */
   float   max;			/* maximum score                   */
   int     sqlen;		/* length of sampled sequences     */
+  sigset_t blocksigs;		/* list of signals to protect from */
 
   int     nsample;		/* number of random seqs to sample */
   int     seed;			/* random number seed              */
-  float   lenmult;		/* length multiplier               */
+  int     fixedlen;		/* fixed length, or 0 if unused    */
+  float   lenmean;		/* mean of length distribution     */
+  float   lensd;		/* std dev of length distribution  */
   char   *histfile;             /* histogram save file             */
   FILE   *hfp;                  /* open file pointer for histfile  */
 
@@ -97,15 +106,19 @@ main(int argc, char **argv)
    ***********************************************/
 
   nsample  = 10000;
-  lenmult  = 2.0;
+  fixedlen = 0;
+  lenmean  = 325.;
+  lensd    = 200.;
   seed     = (int) time ((time_t *) NULL);
   histfile = NULL;
 
   while (Getopt(argc, argv, OPTIONS, NOPTIONS, usage,
 		&optind, &optname, &optarg))
     {
-      if      (strcmp(optname, "-l") == 0) lenmult = atof(optarg);
-      else if (strcmp(optname, "-n") == 0) nsample = atoi(optarg); 
+      if      (strcmp(optname, "-l") == 0) fixedlen = atoi(optarg);
+      else if (strcmp(optname, "-m") == 0) lenmean  = atof(optarg); 
+      else if (strcmp(optname, "-n") == 0) nsample  = atoi(optarg); 
+      else if (strcmp(optname, "-s") == 0) lensd    = atof(optarg); 
       else if (strcmp(optname, "--histfile") == 0) histfile = optarg;
       else if (strcmp(optname, "-h") == 0)
 	{
@@ -127,12 +140,20 @@ main(int argc, char **argv)
   if ((hmmfp = HMMFileOpen(hmmfile, "HMMERDB")) == NULL)
     Die("failed to open HMM file %s for reading.", hmmfile);
 
-				/* generate a tmp file name in hmmfile directory */
+  /* Generate calibrated HMM(s) in a tmp file in the current
+   * directory. When we're finished, we delete the original
+   * HMM file and rename() this one. That way, the worst
+   * effect of a catastrophic failure should be that we
+   * leave a tmp file lying around, but the original HMM
+   * file remains uncorrupted. tmpnam() doesn't work here,
+   * because it'll put the file in /tmp and we won't
+   * necessarily be able to rename() it from there.
+   */
   tmpfile = MallocOrDie(strlen(hmmfile) + 5);
   strcpy(tmpfile, hmmfile);
-  strcat(tmpfile, ".tmp");
+  strcat(tmpfile, ".xxx");	/* could be more inventive here... */
   if (FileExists(tmpfile))
-    Die("temporary file %s already exists, can't open for writing", tmpfile);
+    Die("temporary file %s already exists; please delete it first", tmpfile);
   if (hmmfp->is_binary) mode = "wb";
   else                  mode = "w"; 
   if ((outfp = fopen(tmpfile, mode)) == NULL)
@@ -151,7 +172,12 @@ main(int argc, char **argv)
 
   Banner(stdout, banner);
   printf("HMM file:                 %s\n", hmmfile);
-  printf("Length multiplier:        %.1f\n", lenmult);
+  if (fixedlen) 
+    printf("Length fixed to:          %d\n", fixedlen);
+  else {
+    printf("Length distribution mean: %.0f\n", lenmean);
+    printf("Length distribution s.d.: %.0f\n", lensd);
+  }
   printf("Number of samples:        %d\n", nsample);
   printf("random seed:              %d\n", seed);
   printf("histogram(s) saved to:    %s\n",
@@ -172,12 +198,13 @@ main(int argc, char **argv)
       
       hist = AllocHistogram(-200, 200, 100);
 
-				/* choose length of random sequences */
-      sqlen = lenmult * hmm->M;
-
       max = -FLT_MAX;
       for (idx = 0; idx < nsample; idx++)
 	{
+				/* choose length of random sequence */
+	  if (fixedlen) sqlen = fixedlen;
+	  else do sqlen = (int) Gaussrandom(lenmean, lensd); while (sqlen < 1);
+
 	  seq = RandomSequence(Alphabet, randomseq, Alphabet_size, sqlen);
 	  dsq = DigitizeSequence(seq, sqlen);
 
@@ -192,10 +219,15 @@ main(int argc, char **argv)
 
       /* Fit an EVD to the observed histogram.
        * The TRUE left-censors and fits only the right slope of the histogram.
-       * The 200. is an arbitrary high number that means we won't trim outliers
+       * The 9999. is an arbitrary high number that means we won't trim outliers
        * on the right.
        */
-      ExtremeValueFitHistogram(hist, TRUE, 200.);
+      if (! ExtremeValueFitHistogram(hist, TRUE, 9999.))
+	{ 
+	  fclose(outfp); 
+	  remove(tmpfile);
+	  Die("Failed to fit the histogram; maybe you set -n too small?");
+	}
 
       /* Set HMM EVD parameters 
        */
@@ -211,7 +243,6 @@ main(int argc, char **argv)
       /* Output results
        */
       printf("HMM    : %s\n", hmm->name);
-      printf("sqlen  : %12d\n", sqlen);
       printf("mu     : %12f\n", hmm->mu);
       printf("lambda : %12f\n", hmm->lambda);
       printf("max    : %12f\n", max);
@@ -228,14 +259,19 @@ main(int argc, char **argv)
       FreeHistogram(hist);
     }
 
-  /* If all is well, remove original file and replace it
-   * with the tmpfile. This code should be protected from
-   * interrupt signals.
+  /* Now, carefully remove original file and replace it
+   * with the tmpfile. Note the protection from signals;
+   * we wouldn't want a user to ctrl-C just as we've deleted
+   * their HMM but before the new one is moved.
    */
   HMMFileClose(hmmfp);
-  if (fclose(outfp)   != 0)          Die("fclose() failed on %s", tmpfile);
-  if (remove(hmmfile) != 0)          Die("failed to remove %s",   hmmfile);
-  if (rename(tmpfile, hmmfile) != 0) Die("failed to rename %s to %s", tmpfile, hmmfile); 
+  if (fclose(outfp)   != 0) PANIC;
+  if (sigemptyset(&blocksigs) != 0) PANIC;
+  if (sigaddset(&blocksigs, SIGINT) != 0) PANIC;
+  if (sigprocmask(SIG_BLOCK, &blocksigs, NULL) != 0)   PANIC;
+  if (remove(hmmfile) != 0)                            PANIC;
+  if (rename(tmpfile, hmmfile) != 0)                   PANIC;
+  if (sigprocmask(SIG_UNBLOCK, &blocksigs, NULL) != 0) PANIC;
 
   /***********************************************
    * Exit
