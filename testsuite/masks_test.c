b38929c8bdfaa5f25c21f1ccd6df7714a2c49d18
Sean Eddy <sean@eddylab.org>
1998-02-03 01:06:40 +0000

    exercises code in masks.c
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/trunk/hmmer@143 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- testsuite/masks_test.c
+++ testsuite/masks_test.c
@@ -1,6 +1,5 @@
 /* masks_test.c
  * SRE, Tue Nov 18 11:10:20 1997 [St. Louis]
- * cp masks_test.c ../src/testdriver.c; cd ../src; make testdriver
  * 
  * Test driver for sequence masking routines in masks.c
  * 
@@ -26,15 +25,16 @@ Usage: testdriver [-options]\n\
   Available options are:\n\
   -h              : help; display this usage info\n\
   -v              : verbose output\n\
+";
+
+static char experts[] = "\
   --xnu     <file>: apply xnu to seqs in <file>\n\
-  --miniseg <file>: apply miniseg to seqs in <file>\n\
 \n";
 
 static struct opt_s OPTIONS[] = {
   { "-h",       TRUE,  sqdARG_NONE  },
   { "-v",       TRUE,  sqdARG_NONE  },
   { "--xnu",    FALSE, sqdARG_STRING },
-  { "--miniseg",FALSE, sqdARG_STRING },
 };
 #define NOPTIONS (sizeof(OPTIONS) / sizeof(struct opt_s))
 
@@ -65,7 +65,6 @@ main(int argc, char **argv)
   char *optarg;                 /* argument found by Getopt()               */
   int   optind;                 /* index in argv[]                          */
   int   be_verbose;
-  char *segfile;		/* NULL, or file to run miniseg on */
   char *xnufile;		/* NULL, or file to run xnu on     */
 
 #ifdef MEMDEBUG
@@ -80,22 +79,22 @@ main(int argc, char **argv)
    ***********************************************/
 
   be_verbose = FALSE;
-  segfile    = NULL;
   xnufile    = NULL;
 
   while (Getopt(argc, argv, OPTIONS, NOPTIONS, usage,
                 &optind, &optname, &optarg))  {
     if      (strcmp(optname, "-v")       == 0) { be_verbose = TRUE;   }
-    else if (strcmp(optname, "--miniseg")== 0) { segfile    = optarg; }
     else if (strcmp(optname, "--xnu")    == 0) { xnufile    = optarg; }
     else if (strcmp(optname, "-h")       == 0) {
       Banner(stdout, banner);
       puts(usage);
+      puts(experts);
       exit(0);
     }
   }
   if (argc - optind != 0)
     Die("Incorrect number of arguments.\n%s\n", usage);
+
   SetAlphabet(hmmAMINO);
 
   /* XNU test
@@ -126,7 +125,6 @@ main(int argc, char **argv)
   if (strcmp(answer1, result) != 0)
     Die("XNU test failed.");
   free(result);
-
 
   /* On demand XNU test.
    */
@@ -165,46 +163,6 @@ main(int argc, char **argv)
       SeqfileClose(sqfp);
     }
 
-
-  /* On demand MiniSEG test.
-   * MiniSEG actually doesn't work, but test is retained in case
-   * it is revived...
-   */
-   if (segfile != NULL)
-    {
-      int     format;
-      SQFILE *sqfp;
-      SQINFO  sqinfo;
-      int     xnum;		/* number of x's */
-      float   max;		/* maximum segment score seen */
-      
-      if (! SeqfileFormat(segfile, &format, NULL))
-	switch (squid_errno) {
-	case SQERR_NOFILE: 
-	  Die("Sequence file %s could not be opened for reading", segfile); break;
-	case SQERR_FORMAT: 
-	default:           
-	  Die("Failed to determine format of sequence file %s", segfile);
-	}
-      if ((sqfp = SeqfileOpen(segfile, format, NULL)) == NULL)
-	Die("Failed to open sequence database file %s\n%s\n", segfile, usage);
-
-      while (ReadSeq(sqfp, format, &seq, &sqinfo)) 
-	{
-	  dsq = DigitizeSequence(seq, sqinfo.len);
-	  xnum = MiniSEG(dsq, sqinfo.len, &max);
-	  result = DedigitizeSequence(dsq, sqinfo.len);
-
-	  printf("%-20s\t%5d\t%.1f\n", sqinfo.name, xnum, max);
-	  if (be_verbose)
-	    WriteSeq(stdout, kPearson, result, &sqinfo); 
-
-	  free(dsq);
-	  free(seq);
-	  free(result);
-	}
-      SeqfileClose(sqfp);
-    }
 
 #ifdef MEMDEBUG
   current_size = malloc_inuse(&histid2);
