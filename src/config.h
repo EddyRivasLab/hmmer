1f6f64c80150c52d74e8d1f5839e208c760e7277
Sean Eddy <sean@eddylab.org>
1998-07-16 13:03:02 +0000

    deleted DEBUGLEVEL stuff. This is now part of squid.h.
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/trunk/hmmer@304 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- src/config.h
+++ src/config.h
@@ -1,6 +1,6 @@
 /************************************************************
- * HMMER - Biological sequence analysis with profile-HMMs
- * Copyright (C) 1992-1997 Sean R. Eddy
+ * HMMER - Biological sequence analysis with profile HMMs
+ * Copyright (C) 1992-1998 Washington University School of Medicine
  *
  *   This source code is distributed under the terms of the 
  *   GNU General Public License. See the files COPYING and 
@@ -31,14 +31,12 @@
  * a threads version will parallelize across. This can be overridden
  * by -DHMMER_NCPU=x in the Makefile, and by a setenv HMMER_NCPU x
  * in the environment, and usually by a command line option.
- * By default, we configure for up to a quad-processor system;
- * if the system has fewer processors than that, it's ok, there
- * will just be some negligible overhead costs.
+ * Usually we detect the number of processors dynamically, but
+ * on some systems (FreeBSD and Linux, notably), we can't. On
+ * these systems we assume 2 processors by default. That assumption
+ * can be overridden here if HMMER_NCPU is uncommented.
  */
-#ifndef HMMER_NCPU
-#define HMMER_NCPU 4
-#endif
-
+/* #define HMMER_NCPU 4 */ 
 
 #define INTSCALE    1000.0      /* scaling constant for floats to integer scores   */
 #define MAXABET     20	        /* maximum size of alphabet (4 or 20)              */
@@ -49,19 +47,6 @@
 #define NXRAY       4           /* number of structural inputs                */
 #define LOGSUM_TBL  20000       /* controls precision of Logsum()             */
 #define ALILENGTH   50		/* length of displayed alignment lines        */
-
-/* Debugging levels in HMMER are controlled by conditional preprocessing
- */
-#ifndef DEBUGLEVEL
-#define DEBUGLEVEL 0
-#endif
-#define DEBUG_NONE        0	/* no debugging output                          */
-#define DEBUG_LIGHT       1	/* turn on most assertions                      */
-#define DEBUG_SOME        2	/* turn on some output; low verbosity           */
-#define DEBUG_AGGRESSIVE  3	/* turn on all assertions; mild verbosity       */
-#define DEBUG_LOTS        4	/* turn on most output; moderate/high verbosity */
-#define DEBUG_EXTREME     5	/* turn on all output; intolerable verbosity    */
-
 
 #endif /*CONFIGH_INCLUDED*/
 
