0b80d94ae63e61ab848a440db2f62f38df8e3424
Sean Eddy <sean@eddylab.org>
1999-05-02 22:38:57 +0000

    updated to 2.1.2
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/trunk/hmmer@466 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- src/version.h
+++ src/version.h
@@ -8,13 +8,13 @@
  */
 
 
-#define RELEASE     "2.1.1"
-#define RELEASEDATE "Dec 1998"
+#define RELEASE     "2.1.2"
+#define RELEASEDATE "May 1999"
 
 #ifndef LINTING			/* compiled-in stamp for all binaries  */
 static char *copyright = "\
    HMMER -- Biological sequence analysis with profile HMMs.\n\
-   Copyright (C) 1992-1998 Washington University School of Medicine.\n\
+   Copyright (C) 1992-1999 Washington University School of Medicine.\n\
 \n\
    HMMER is distributed under the terms of the GNU General Public\n\
    License, unless otherwise stated; see the files COPYING and LICENSE\n\
