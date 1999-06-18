b6a9dba8bc50b1f1b339b1878b81073a9962f66f
Sean Eddy <sean@eddylab.org>
1999-06-18 02:06:39 +0000

    added storage of accession number (acc) alongside name, desc for a hit: support for hmmpfam --acc option added today
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/trunk/hmmer@482 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- src/tophits.c
+++ src/tophits.c
@@ -43,7 +43,7 @@
  * Estimated storage per hit: 
  *        coords:   16 bytes
  *        scores:    8 bytes
- *     name/desc:  192 bytes
+ * name/acc/desc:  192 bytes
  *     alignment: 1000 bytes   total = ~1200 bytes with alignment;
  *                                   = ~200 bytes without  
  *     Designed for: 10^5 hits (20 MB) or 10^4 alignments (10 MB)  
@@ -52,13 +52,8 @@
 #include <string.h>
 #include <float.h>
 #include <limits.h>
-
 #include "structs.h"
 #include "funcs.h"
-
-#ifdef MEMDEBUG
-#include "dbmalloc.h"
-#endif
 
 /* Function: AllocTophits()
  * 
@@ -96,6 +91,7 @@ FreeTophits(struct tophit_s *h)
     {
       if (h->unsrt[pos].ali  != NULL) FreeFancyAli(h->unsrt[pos].ali);
       if (h->unsrt[pos].name != NULL) free(h->unsrt[pos].name);
+      if (h->unsrt[pos].acc  != NULL) free(h->unsrt[pos].acc);
       if (h->unsrt[pos].desc != NULL) free(h->unsrt[pos].desc);
     }
   free(h->unsrt);
@@ -139,7 +135,7 @@ FreeFancyAli(struct fancyali_s *ali)
  *           Caller should not free them; they will be free'd by
  *           the FreeTophits() call. 
  *
- *           In contrast, "name" and "desc" are copied, so caller
+ *           In contrast, "name", "acc", and "desc" are copied, so caller
  *           is still responsible for these.
  *           
  *           Number of args is unwieldy.
@@ -150,8 +146,9 @@ FreeFancyAli(struct fancyali_s *ali)
  *           score    - score of this hit
  *           motherp  - P-value of parent whole sequence
  *           mothersc - score of parent whole sequence 
- *           name     - name of target sequence 
- *           desc     - description of target sequence 
+ *           name     - name of target  
+ *           acc      - accession of target (may be NULL)
+ *           desc     - description of target (may be NULL) 
  *           sqfrom   - 1..L pos in target seq  of start
  *           sqto     - 1..L pos; sqfrom > sqto if rev comp
  *           sqlen    - length of sequence, L
@@ -168,7 +165,7 @@ FreeFancyAli(struct fancyali_s *ali)
 void
 RegisterHit(struct tophit_s *h, double key, 
 	    double pvalue, float score, double motherp, float mothersc,
-	    char *name, char *desc, 
+	    char *name, char *acc, char *desc, 
 	    int sqfrom, int sqto, int sqlen,
 	    int hmmfrom, int hmmto, int hmmlen, 
 	    int domidx, int ndom,
@@ -179,6 +176,7 @@ RegisterHit(struct tophit_s *h, double key,
   if (h->num == h->alloc) GrowTophits(h);
 
   h->unsrt[h->num].name    = Strdup(name);
+  h->unsrt[h->num].acc     = Strdup(acc);
   h->unsrt[h->num].desc    = Strdup(desc);
   h->unsrt[h->num].sortkey = key;
   h->unsrt[h->num].pvalue  = pvalue;
@@ -205,14 +203,14 @@ RegisterHit(struct tophit_s *h, double key,
  *           Any of the data ptrs may be passed as NULL for fields
  *           you don't want. hitlist must have been sorted first.
  *           
- *           name, desc, and ali are returned as pointers, not copies;
+ *           name, acc, desc, and ali are returned as pointers, not copies;
  *           don't free them!
  */ 
 void
 GetRankedHit(struct tophit_s *h, int rank, 
 	     double *r_pvalue, float *r_score, 
 	     double *r_motherp, float *r_mothersc,
-	     char **r_name, char **r_desc,
+	     char **r_name, char **r_acc, char **r_desc,
 	     int *r_sqfrom, int *r_sqto, int *r_sqlen,
 	     int *r_hmmfrom, int *r_hmmto, int *r_hmmlen,
 	     int *r_domidx, int *r_ndom,
@@ -223,6 +221,7 @@ GetRankedHit(struct tophit_s *h, int rank,
   if (r_motherp != NULL) *r_motherp = h->hit[rank]->motherp;
   if (r_mothersc!= NULL) *r_mothersc= h->hit[rank]->mothersc;
   if (r_name    != NULL) *r_name    = h->hit[rank]->name;
+  if (r_acc     != NULL) *r_acc     = h->hit[rank]->acc;
   if (r_desc    != NULL) *r_desc    = h->hit[rank]->desc;
   if (r_sqfrom  != NULL) *r_sqfrom  = h->hit[rank]->sqfrom;
   if (r_sqto    != NULL) *r_sqto    = h->hit[rank]->sqto;
@@ -331,6 +330,8 @@ TophitsReport(struct tophit_s *h, double E, int nseq)
     {
       if (h->unsrt[i].name != NULL)
 	memused += strlen(h->unsrt[i].name) + 1;
+      if (h->unsrt[i].acc != NULL)
+	memused += strlen(h->unsrt[i].acc)  + 1;
       if (h->unsrt[i].desc != NULL)
 	memused += strlen(h->unsrt[i].desc) + 1;
       if (h->unsrt[i].ali != NULL)
