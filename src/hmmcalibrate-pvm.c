b752ead954bdba057beb057221db90d1d1afe891
Sean Eddy <sean@eddylab.org>
1999-12-04 22:44:37 +0000

    Now supports parallelization even for a single model
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/trunk/hmmer@542 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- src/hmmcalibrate-pvm.c
+++ src/hmmcalibrate-pvm.c
@@ -4,8 +4,22 @@
 
 #ifdef HMMER_PVM
 
-/* hmmcalibrate-slave.c
+/* hmmcalibrate-pvm.c
  * SRE, Tue Aug 18 15:19:28 1998
+ * Redesigned for better parallelization: SRE, Wed Dec  1 09:48:58 1999
+ *
+ * Design:
+ *   Initialization: 
+ *       receive parameters of random sequence synthesis, and an HMM.
+ *       send an OK signal to the master.
+ *   
+ *   Main loop:
+ *       receive work packet: # of seqs to make
+ *       Synthesize and score # seqs
+ *       send results: # raw scores.
+ *   
+ *   Termination: 
+ *       master sends a shutdown signal instead of a work packet.
  * 
  * PVM slave for hmmcalibrate.
  * RCS $Id$
@@ -22,10 +36,7 @@
 #include "funcs.h"		/* function declarations                */
 #include "globals.h"		/* alphabet global variables            */
 #include "squid.h"		/* general sequence analysis library    */
-
-#ifdef MEMDEBUG
-#include "dbmalloc.h"
-#endif
+#include "stopwatch.h"		/* CPU timing routines                  */
 
 static void leave_pvm(void);
 
@@ -35,43 +46,54 @@ main(void)
   int      master_tid;		/* PVM TID of our master */
   int      slaveidx;		/* my slave index (0..nslaves-1) */
   struct plan7_s *hmm;		/* HMM to calibrate, sent from master */
-  struct histogram_s *hist;     /* score histogram */
-  int      hmmidx;		/* index of this HMM */
   char    *seq;			/* synthetic random sequence */
   char    *dsq;			/* digitized seq */
   int      len;			/* length of seq */
-  float    sc;			/* score of seq aligned to HMM */
-  float    max;			/* maximum score seen in sample */
+  float   *sc;			/* scores of seqs */
   int      seed;		/* random number seed */
   int      nsample;		/* number of seqs to sample */
   int      fixedlen;		/* if nonzero, fixed length of seq */
   float    lenmean;		/* Gaussian mean length of seq */
   float    lensd;		/* Gaussian length std. dev. for seq */
-  int      fitok;		/* TRUE if EVD fit was OK */
   float    randomseq[MAXABET];	/* iid frequencies of residues */
   float    p1;
   int      alphatype;		/* alphabet type, hmmAMINO or hmmNUCLEIC    */
   int      idx;
   int      code;
+  Stopwatch_t stopwatch;       /* CPU timings */
 
   /* Register leave_pvm() cleanup function so any exit() call
    * first calls pvm_exit().
    */
-  if (atexit(leave_pvm) != 0) { pvm_exit(); Die("slave couldn't register leave_pvm()"); }
+  if (atexit(leave_pvm) != 0) { 
+    pvm_exit(); Die("slave couldn't register leave_pvm()"); 
+  }
 
   /*****************************************************************
    * initialization.
-   * Master broadcasts the problem to us: parameters of the
-   * HMM calibration.  
+   * Master broadcasts the problem to us: 
+   *    an HMM;
+   *    parameters of the HMM calibration.
+   * We send back:
+   *    an OK flag, and our RELEASE, for some sanity checking.  
    ******************************************************************/
+  
+  StopwatchStart(&stopwatch);
 
   master_tid = pvm_parent();	/* who's our master? */
 
   pvm_recv(master_tid, HMMPVM_INIT);
-  pvm_upkint(&nsample,  1, 1);
-  pvm_upkint(&fixedlen, 1, 1);
-  pvm_upkfloat(&lenmean,  1, 1);
-  pvm_upkfloat(&lensd,    1, 1);
+  pvm_upkfloat(&lenmean,  1, 1); /* mean length of random seqs    */
+  pvm_upkfloat(&lensd,    1, 1); /* std. dev. of random seq len   */
+  pvm_upkint(&fixedlen,   1, 1); /* if non-zero, override lenmean */
+  pvm_upkint(&alphatype,  1, 1); /* alphabet type, hmmAMINO or hmmNUCLEIC */
+  pvm_upkint(&seed,       1, 1); /* random number seed */
+  SetAlphabet(alphatype);	 /* must set alphabet before reading HMM! */
+  hmm = PVMUnpackHMM();
+  if (hmm == NULL) Die("oh no, the HMM never arrived");
+
+  P7DefaultNullModel(randomseq, &p1);
+  P7Logoddsify(hmm, TRUE);
 
   /* tell the master we're OK and ready to go (or not)
    */
@@ -83,30 +105,26 @@ main(void)
 
   /*****************************************************************
    * Main loop.
-   * Receive a random number seed, then an HMM to search against.
-   * If we receive a -1 seed, we shut down. 
+   * Receive: a number of sequences we're supposed to do.
+   *          If we receive a 0, we have no work, so wait for shutdown;
+   *          if we receive a -1, shut down.
    *****************************************************************/ 
-  
   slaveidx = -1;
   for (;;) 
     {
       pvm_recv(master_tid, HMMPVM_WORK);
-      pvm_upkint(&seed, 1, 1);
-      if (seed == -1) break;	/* shutdown signal */
-      pvm_upkint(&hmmidx, 1, 1);
-      pvm_upkint(&alphatype,1, 1);
-      SetAlphabet(alphatype);
-      hmm = PVMUnpackHMM();
-      if (hmm == NULL) Die("oh no, the HMM never arrived");
+      pvm_upkint(&nsample,  1, 1);
+      pvm_upkint(&idx,      1, 1);
 
-      if (slaveidx == -1) slaveidx = hmmidx; 
-      P7DefaultNullModel(randomseq, &p1);
+      if (nsample == 0)  continue;  /* go into stasis */
+      if (nsample == -1) break;	    /* shut down      */
 
-      sre_srandom(seed);
-      P7Logoddsify(hmm, TRUE);
-      hist = AllocHistogram(-200, 200, 100);
-      max  = -FLT_MAX;
+      if (slaveidx == -1) {	/* first time: set id, seed sre_random  */
+	slaveidx = idx;
+	sre_srandom(seed+idx);	/* unique seed in current PVM   */
+      }
 
+      sc = MallocOrDie(sizeof(float) * nsample);
       for (idx = 0; idx < nsample; idx++)
 	{
   				/* choose length of random sequence */
@@ -115,50 +133,50 @@ main(void)
 				/* generate it */
 	  seq = RandomSequence(Alphabet, randomseq, Alphabet_size, len);
 	  dsq = DigitizeSequence(seq, len);
+	  SQD_DPRINTF2(("slave %d seq: %d : %20.20s...\n", slaveidx, len, seq));
 
 	  if (P7ViterbiSize(len, hmm->M) <= RAMLIMIT)
-	    sc = P7Viterbi(dsq, len, hmm, NULL);
+	    sc[idx] = P7Viterbi(dsq, len, hmm, NULL);
 	  else
-	    sc = P7SmallViterbi(dsq, len, hmm, NULL);
-
-	  AddToHistogram(hist, sc);
-	  if (sc > max) max = sc;
+	    sc[idx] = P7SmallViterbi(dsq, len, hmm, NULL);
 	  
 	  free(seq);
 	  free(dsq);
 	}
 
-      /* Fit an EVD to the observed histogram.
-       * The TRUE left-censors and fits only the right slope of the histogram.
-       * The 9999. is an arbitrary high number that means we won't trim outliers
-       * on the right.
-       */
-      fitok = ExtremeValueFitHistogram(hist, TRUE, 9999.);
-
-      /* Return output to master.
-       * Currently we don't send the histogram back, but we could.
+      /* Return output to master, some of which is sanity checking.
+       *   1. our slave index.
+       *   2. how many seqs we simulated.
+       *   3. the array of scores we got, so the master can stuff
+       *      them into a histogram.
        */
       pvm_initsend(PvmDataDefault);
       pvm_pkint(&slaveidx, 1, 1);
-      pvm_pkint(&hmmidx, 1, 1);	
-      PVMPackString(hmm->name);
-      pvm_pkint(&fitok,  1, 1);
-      pvm_pkfloat(&(hist->param[EVD_MU]), 1, 1);
-      pvm_pkfloat(&(hist->param[EVD_LAMBDA]), 1, 1);
-      pvm_pkfloat(&max, 1, 1);
+      pvm_pkint(&nsample,  1, 1);
+      pvm_pkfloat(sc, nsample,1); 
       pvm_send(master_tid, HMMPVM_RESULTS);
 
       /* cleanup
        */
-      FreeHistogram(hist);
-      FreePlan7(hmm);
+      free(sc);
     }
 
   /*********************************************** 
    * Cleanup, return.
    ***********************************************/
+  
+  FreePlan7(hmm);
+  StopwatchStop(&stopwatch);
 
-  return 0;			/* pvm_exit() is called by atexit() registration. */
+  /* tell the master we heard his shutdown signal, and
+   * give him our CPU times; then exit.
+   */
+  pvm_initsend(PvmDataDefault);
+  pvm_pkint(&slaveidx, 1, 1);	
+  StopwatchPVMPack(&stopwatch);
+  pvm_send(master_tid, HMMPVM_RESULTS);  
+
+  return 0;	/* pvm_exit() is called by atexit() registration. */
 }
 
 /* Function: leave_pvm()
@@ -178,7 +196,7 @@ void leave_pvm(void)
 #include <stdio.h>
 int main(void)
 {
-  printf("hmmcalibrate-slave is disabled. PVM support was not compiled into HMMER.\n");
+  printf("hmmcalibrate-pvm disabled. PVM support was not compiled into HMMER.\n");
   exit(0);
 } 
 
