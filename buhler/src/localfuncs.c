528bf65c9ea77334d937603fd3ca3492c1162c6a
Christopher Swope <crs2@wustl.edu>
2005-08-12 01:49:18 +0000

    Builds: Default - Yes; Altivec - Yes; Buggy : No* *Produces same results as trunk
    I forgot to add customfuncs.h to the repository with my last commit, so I am adding it now.
    
    I have also added the files localstructs.h, localstructs.c and localfuncs.c.  These files should allow someone to easily create a local customized version of HMMER using the new customizable architecture.  They contain empty definitions for the structures and functions that are part of the customizable interface.  Each structure and function has also been (roughly) documented.
    
    
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/hmmer/branches@1432 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

---
+++ buhler/src/localfuncs.c
@@ -0,0 +1,124 @@
+#include "structs.h"
+#include "funcs.h"
+/* Function:  ViterbiSpaceOK()
+ * Incept:    SRE, Wed Oct  1 12:53:13 2003 [St. Louis]
+ *
+ * Purpose:   Returns TRUE if the existing matrix allocation
+ *            is already sufficient to hold the requested MxN, or if
+ *            the matrix can be expanded in M and/or N without
+ *            exceeding RAMLIMIT megabytes. 
+ *            
+ *            This gets called anytime we're about to do P7Viterbi().
+ *            If it returns FALSE, we switch into the appropriate
+ *            small-memory alternative: P7SmallViterbi() or
+ *            P7WeeViterbi().
+ *            
+ *            Checking the DP problem size against P7ViterbiSize()
+ *            is not enough, because the DP matrix may be already
+ *            allocated in MxN. For example, if we're already
+ *            allocated to maxM,maxN of 1447x979, and we try to
+ *            do a problem of MxN=12x125000, P7ViterbiSize() may
+ *            think that's fine - but when we resize, we can only
+ *            grow, so we'll expand to 1447x125000, which is 
+ *            likely over the RAMLIMIT. [bug#h26; xref SLT7 p.122]
+ *
+ * Args:      
+ *
+ * Returns:   TRUE if we can run P7Viterbi(); FALSE if we need
+ *            to use a small memory variant.
+ *
+ * Xref:      STL7 p.122.
+ */
+extern int   ViterbiSpaceOK(int L, int M, struct dpmatrix_s *mx){
+
+}
+
+/* Function: Backward()
+ * 
+ * Purpose:  The Backward dynamic programming algorithm.
+ *           The scaling issue is dealt with by working in log space
+ *           and calling ILogsum(); this is a slow but robust approach.
+ *           
+ * Args:     dsq    - sequence in digitized form
+ *           L      - length of dsq
+ *           hmm    - the model
+ *           ret_mx - RETURN: dp matrix; pass NULL if it's not wanted
+ *           
+ * Return:   log P(S|M)/P(S|R), as a bit score.
+ */
+extern float Backward(unsigned char *dsq, int L, struct plan7_s *hmm,	
+		      struct dpmatrix_s **ret_mx){
+
+}
+
+/* Function: P7Forward()
+ * 
+ * Note:     This was originally defined as P7Forward() in core_algorithms.c, 
+ *           but it is implementation dependent, since it accesses the 
+ *           customized dpmatrix structure.  So I moved it here and renamed it 
+ *           with a more generic name that fits into the new architecture.
+ *             - CRS 15 July 2005
+ *
+ * Purpose:  The Forward dynamic programming algorithm.
+ *           The scaling issue is dealt with by working in log space
+ *           and calling ILogsum(); this is a slow but robust approach.
+ *           
+ * Args:     dsq    - sequence in digitized form
+ *           L      - length of dsq
+ *           hmm    - the model
+ *           ret_mx - RETURN: dp matrix; pass NULL if it's not wanted
+ *           
+ * Return:   log P(S|M)/P(S|R), as a bit score.
+ */
+extern float Forward(unsigned char *dsq, int L, struct plan7_s *hmm, 
+		     struct dpmatrix_s **ret_mx){
+
+}
+
+/* Function: Viterbi()
+ *
+ * Purpose:  The Viterbi dynamic programming algorithm. 
+ *           Identical to Forward() except that max's
+ *           replace sum's. 
+ *           
+ *           This is the slower, more understandable version
+ *           of P7Viterbi(). The default version in fastfuncs.c
+ *           is portably optimized and more difficult to understand;
+ *           the ALTIVEC version in altivecfuncs.c is vectorized
+ *           with Altivec-specific code, and is pretty opaque.
+ *           
+ *           This function is not enabled by default; it is only
+ *           activated by -DSLOW at compile time.
+ *           
+ * Args:     dsq    - sequence in digitized form
+ *           L      - length of dsq
+ *           hmm    - the model
+ *           mx     - reused DP matrix
+ *           ret_tr - RETURN: traceback; pass NULL if it's not wanted
+ *           
+ * Return:   log P(S|M)/P(S|R), as a bit score
+ */
+extern float Viterbi(unsigned char *dsq, int L, struct plan7_s *hmm, 
+		     struct dpmatrix_s *mx, struct p7trace_s **ret_tr){
+
+}
+
+/* Function: ViterbiTrace()
+ * Date:     SRE, Sat Aug 23 10:30:11 1997 (St. Louis Lambert Field) 
+ *
+ * Purpose:  Traceback of a Viterbi matrix: i.e. retrieval 
+ *           of optimum alignment.
+ *           
+ * Args:     hmm    - hmm, log odds form, used to make mx
+ *           dsq    - sequence aligned to (digital form) 1..N  
+ *           N      - length of seq
+ *           mx     - the matrix to trace back in, N x hmm->M
+ *           ret_tr - RETURN: traceback.
+ *           
+ * Return:   (void)
+ *           ret_tr is allocated here. Free using P7FreeTrace().
+ */
+extern void  ViterbiTrace(struct plan7_s *hmm, unsigned char *dsq, int N,
+			  struct dpmatrix_s *mx, struct p7trace_s **ret_tr){
+
+}
---
+++ buhler/src/localstructs.c
@@ -0,0 +1,150 @@
+#include "structs.h"
+
+/*
+ * Function: AllocLogoddsShell()
+ * Date:     CRS, 10 June 2005
+ * 
+ * Purpose:  Allocates the memory needed by the shell of the logodds_s
+ *           structure.  Called from within AllocPlan7Shell().
+ *
+ * Args:     hmm - the profile hmm containing the logodds_s structure
+ *
+ * Returns:  (void)
+ *           Memory is allocated for the shell of hmm->lom.  Freed 
+ *           from FreeLogodds(), which is called from within FreePlan7().
+ */
+inline void AllocLogoddsShell(struct plan7_s *hmm){ 
+  /*
+   * Uncomment the following line if your implementation does not use
+   * the logodds_s structure.
+   */
+  /* hmm->lom = NULL; */
+}
+
+/*
+ * Function: AllocLogoddsBody()
+ * Date:     CRS, 10 June 2005
+ *
+ * Purpose:  Allocates the memory needed by the logodds_s strcuture.
+ *           Called from within AllocPlan7Body().
+ *
+ * Args:     hmm - the profile hmm containing the logodds_s structure
+ *
+ * Returns:  (void)
+ *           Memory is allocated for  hmm->lom.  Freed from FreeLogodds(), 
+ *           which is called from within FreePlan7().
+ */
+inline void AllocLogoddsBody(struct plan7_s *hmm){
+
+}
+
+/*
+ * Function: FreeLogodds()
+ * Date:     CRS, 10 June 2005
+ *
+ * Purpose:  Frees ALL memory (shell and body) used by the logodds_s
+ *           structure.  Called from FreePlan7().
+ * 
+ * Args:     hmm - the profile hmm containing the logodds_s structure
+ *
+ * Returns:  (void)
+ *           Memory used by hmm->lom is freed.  
+ */
+inline void FreeLogodds(struct plan7_s *hmm){
+
+}
+
+/*
+ * Function: FillCustomLogodds()
+ * Date:     CRS, 13 July 2005
+ *
+ * Purpose:  Fills the custom_s logodds structure using the data from the
+ *           profile hmm.  One can assume all non-customized values of the 
+ *           hmm have been filled in before this function is called.  No
+ *           memory should be allocated here; that should be done in
+ *           AllocLogoddsBody().
+ *
+ * Args:     hmm - the profile hmm containing the logodds_s structure
+ *
+ * Returns:  (void)
+ */
+inline void FillCustomLogodds(struct plan7_s *hmm){
+
+}
+
+/* Function: CreateDPMatrix()
+ *
+ * Purpose:  Create a dynamic programming matrix for standard Forward,
+ *           Backward, or Viterbi, with scores kept as scaled log-odds
+ *           integers. Keeps 2D arrays compact in RAM in an attempt 
+ *           to maximize cache hits. 
+ *           
+ *           The mx structure can be dynamically grown, if a new
+ *           HMM or seq exceeds the currently allocated size. Dynamic
+ *           growing is more efficient than an alloc/free of a whole
+ *           matrix for every new target. The ResizePlan7Matrix()
+ *           call does this reallocation, if needed. Here, in the
+ *           creation step, we set up some pads - to inform the resizing
+ *           call how much to overallocate when it realloc's. 
+ *           
+ * Args:     N     - N+1 rows are allocated, for sequence.  
+ *           M     - size of model in nodes
+ *           padN  - over-realloc in seq/row dimension, or 0
+ *           padM  - over-realloc in HMM/column dimension, or 0
+ *                 
+ * Return:   mx
+ *           mx is allocated here. Caller frees with FreeDPMatrix(mx).
+ */
+inline struct dpmatrix_s *CreateDPMatrix(int N, int M, int padN, int padM){
+
+}
+
+/* Function: ResizeDPMatrix()
+ * 
+ * Purpose:  Reallocate a dynamic programming matrix, if necessary,
+ *           for a problem of NxM: sequence length N, model size M.
+ *           (N=1 for small memory score-only variants; we allocate
+ *           N+1 rows in the DP matrix.) 
+ *           
+ *           We know (because of the way hmmsearch and hmmpfam are coded)
+ *           that only one of the two dimensions is going to change
+ *           in size after the first call to ResizePlan7Matrix();
+ *           that is, for hmmsearch, we have one HMM of fixed size M
+ *           and our target sequences may grow in N; for hmmpfam,
+ *           we have one sequence of fixed size N and our target models
+ *           may grow in M. What we have to watch out for is P7SmallViterbi()
+ *           working on a divide and conquer problem and passing us N < maxN,
+ *           M > maxM; we should definitely *not* reallocate a smaller N.
+ *           Since we know that only one dimension is going to grow,
+ *           we aren't scared of reallocating to maxN,maxM. (If both
+ *           M and N could grow, we would be more worried.)
+ *
+ *           Returns individual ptrs to the four matrix components
+ *           as a convenience.
+ *           
+ * Args:     mx    - an already allocated model to grow.
+ *           N     - seq length to allocate for; N+1 rows
+ *           M     - size of model
+ *           xmx, mmx, imx, dmx 
+ *                 - RETURN: ptrs to four mx components as a convenience
+ *                   
+ * Return:   (void)
+ *           mx is (re)allocated here.
+ */
+inline void ResizeDPMatrix(struct dpmatrix_s *mx, int N, int M){
+
+}
+
+/* Function: FreeDPMatrix()
+ *
+ * Note:     Originally defined as FreePlan7Matrix() in core_algorithms.c,
+ *           but was moved here to match the new architecture.
+ *             - CRS 21 June 2005
+ * 
+ * Purpose:  Free a dynamic programming matrix allocated by CreateDPMatrix().
+ * 
+ * Return:   (void)
+ */
+inline void FreeDPMatrix(struct dpmatrix_s *mx){
+
+}
