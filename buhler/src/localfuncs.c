533fe778561dcfda52bb1e287d04f7e38c0ecd7e
Jeremy Buhler <jbuhler@wustl.edu>
2006-01-03 16:45:38 +0000

    updates to comments by JB 1/3/2006
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/hmmer/branches@1538 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- buhler/src/localfuncs.c
+++ buhler/src/localfuncs.c
@@ -1,5 +1,6 @@
 #include "structs.h"
 #include "funcs.h"
+
 /* Function:  ViterbiSpaceOK()
  * Incept:    SRE, Wed Oct  1 12:53:13 2003 [St. Louis]
  *
@@ -8,7 +9,7 @@
  *            the matrix can be expanded in M and/or N without
  *            exceeding RAMLIMIT megabytes. 
  *            
- *            This gets called anytime we're about to do P7Viterbi().
+ *            This gets called anytime we're about to do Viterbi().
  *            If it returns FALSE, we switch into the appropriate
  *            small-memory alternative: P7SmallViterbi() or
  *            P7WeeViterbi().
@@ -22,15 +23,39 @@
  *            grow, so we'll expand to 1447x125000, which is 
  *            likely over the RAMLIMIT. [bug#h26; xref SLT7 p.122]
  *
- * Args:      
+ * Args:      L  - length of sequence
+ *            M  - length of HMM
+ *            mx - an allocated model
  *
- * Returns:   TRUE if we can run P7Viterbi(); FALSE if we need
+ * Returns:   TRUE if we can run Viterbi(); FALSE if we need
  *            to use a small memory variant.
  *
  * Xref:      STL7 p.122.
  */
 extern int   ViterbiSpaceOK(int L, int M, cust_dpmatrix_s *mx){
 
+}
+
+/*
+ * Function: DispatchViterbi()
+ * Date:     CRS, Wed 18 Aug 2005 [J. Buhler's student, St. Louis]
+ *
+ * Purpose:  Determines the appropriate Viterbi algorithm to call,
+ *           based on the values of the parameters provided.  
+ *
+ * Args:     dsq    - sequence in digitized form
+ *           L      - length of dsq
+ *           hmm    - the model
+ *           mx     - reused dp matrix
+ *           ret_tr - RETURN: traceback; pass NULL if it's not wanted,
+                      see below
+ *           need_trace - true if traceback is needed, false otherwise
+ *
+ * Return:   log P(S|M)/P(S|R), as a bit score
+ */
+float DispatchViterbi(unsigned char *dsq, int L, struct plan7_s *hmm,
+		      cust_dpmatrix_s *mx, struct p7trace_s **ret_tr,
+		      int need_trace){
 }
 
 /* Function: Backward()
@@ -51,7 +76,7 @@ extern float Backward(unsigned char *dsq, int L, struct plan7_s *hmm,
 
 }
 
-/* Function: P7Forward()
+/* Function: Forward()
  *
  * Purpose:  The Forward dynamic programming algorithm.
  *           The scaling issue is dealt with by working in log space
@@ -73,16 +98,7 @@ extern float Forward(unsigned char *dsq, int L, struct plan7_s *hmm,
  *
  * Purpose:  The Viterbi dynamic programming algorithm. 
  *           Identical to Forward() except that max's
- *           replace sum's. 
- *           
- *           This is the slower, more understandable version
- *           of P7Viterbi(). The default version in fastfuncs.c
- *           is portably optimized and more difficult to understand;
- *           the ALTIVEC version in altivecfuncs.c is vectorized
- *           with Altivec-specific code, and is pretty opaque.
- *           
- *           This function is not enabled by default; it is only
- *           activated by -DSLOW at compile time.
+ *           replace sum's.
  *           
  * Args:     dsq    - sequence in digitized form
  *           L      - length of dsq
@@ -94,25 +110,5 @@ extern float Forward(unsigned char *dsq, int L, struct plan7_s *hmm,
  */
 extern float Viterbi(unsigned char *dsq, int L, struct plan7_s *hmm, 
 		     cust_dpmatrix_s *mx, struct p7trace_s **ret_tr){
-
-}
-
-/* Function: ViterbiTrace()
- * Date:     SRE, Sat Aug 23 10:30:11 1997 (St. Louis Lambert Field) 
- *
- * Purpose:  Traceback of a Viterbi matrix: i.e. retrieval 
- *           of optimum alignment.
- *           
- * Args:     hmm    - hmm, log odds form, used to make mx
- *           dsq    - sequence aligned to (digital form) 1..N  
- *           N      - length of seq
- *           mx     - the matrix to trace back in, N x hmm->M
- *           ret_tr - RETURN: traceback.
- *           
- * Return:   (void)
- *           ret_tr is allocated here. Free using P7FreeTrace().
- */
-extern void  ViterbiTrace(struct plan7_s *hmm, unsigned char *dsq, int N,
-			  cust_dpmatrix_s *mx, struct p7trace_s **ret_tr){
 
 }
