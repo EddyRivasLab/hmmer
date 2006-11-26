2df89034d0ff43d94dae5bdc7c20f46c03e6a86f
Sean Eddy <sean@eddylab.org>
2006-11-26 16:33:51 +0000

    created Infernal 0.71 release branch
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/hmmer/branches@1711 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

---
+++ infernal/0.71/src/p7_dp_fast.h
@@ -0,0 +1,21 @@
+/* p7_dp_fast.h
+ * 
+ * Profile and DP matrix (P7_OPROFILE, P7_OMX) structures
+ * for the "fast" (standard optimized) implementation. This
+ * implementation simply uses the generic structures.
+ * 
+ * SVN $Id$
+ * SRE, Tue Apr 11 12:44:35 2006
+ */
+#ifndef P7_DPFAST_INCLUDED
+#define P7_DPFAST_INCLUDED
+
+#include "hmmer.h"
+
+typedef P7_PROFILE P7_OPROFILE;
+typedef P7_GMX     P7_OMX;
+
+#endif /*P7_DPFAST_INCLUDED*/
+/*****************************************************************
+ * @LICENSE@
+ *****************************************************************/
