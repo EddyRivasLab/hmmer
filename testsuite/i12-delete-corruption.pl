EddyRivasLab/hmmer

commit 0bc726a26f3a689854d04f15b6b2523bfd448a0d

Fixes bugs and documentation errors in handling X states (sequence fragments, internal entry/exit in core traces). Detected by Elena.
tracealign.c:map_new_msa() wasn't constructing matmap[] correctly
with transitions involving X states.

tracealign.c:make_digital_msa() and make_text_msa() have to handle
X state entry/exit by setting apos appropriately - in particular on
B->X->Ik internal entry to an insert state.

p7_trace.c:p7_trace_Validate() now checks X states carefully.

p7_profile_GetT(): return 0.0 as "score" for transition involving X state.

hmmer.h: revise doc on P7_TRACE; X transitions can be B->X->{MDI}k and
{MDI}k->X->E, not just B->X->Mk and Mk->X->E. I was thinking of glocal
wing retraction when I wrote that, not model construction.

Speaking of wing retraction, there are still problems with X state
handling in wing retraction. See p7_trace.c::p7_trace_Count(), which
apparently expects X states to occur in local alignment traces, so it
can distinguish glocal alignment cases it needs to undo wing
retraction. We're not doing glocal much yet, so this issue isn't
pressing, but don't forget.

Added unit test utest_fragments() in build.c to test for bugs like this. 
Revised/updated the unit test driver.

Added an example driver to build.c, to dump traces and see the
internals of what's happening in build.c functions.

Also: fixed two bad status == eslOK no-op typos that Michael
introduced in evalues.c.



git-svn-id: https://svn.janelia.org/eddylab/eddys/src/hmmer/trunk@3041 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

src/tracealign.c
@@ -277,6 +277,7 @@ map_new_msa(P7_TRACE **tr, int nseq, int M, int optflags, int **ret_inscount,
   for (idx = 0; idx < nseq; idx++) 
     {
       nins = 0;
+      k    = 0;
       for (z = 1; z < tr[idx]->N; z++) 
 	{
 	  switch (tr[idx]->st[z]) {
@@ -285,16 +286,24 @@ map_new_msa(P7_TRACE **tr, int nseq, int M, int optflags, int **ret_inscount,
 	  case p7T_C: if (tr[idx]->st[z-1] == p7T_C) nins++; break;
 	    
 	  case p7T_M: /* M,D: record max. reset ctr; M only: set matuse[] */
-	    matuse[tr[idx]->k[z]] = TRUE;
-	    /* fallthru */
+	    k             = tr[idx]->k[z];	/* k++ doesn't work. May be a B->X->Mk fragment entry */
+	    inscount[k-1] = ESL_MAX(nins, inscount[k-1]); 
+	    matuse[k]     = TRUE;
+	    nins          = 0;
+	    break;
+
 	  case p7T_D: /* Can handle I->D transitions even though currently not in H3 models */
-	    inscount[tr[idx]->k[z]-1] = ESL_MAX(nins, inscount[tr[idx]->k[z]-1]); 
-	    nins = 0;
+	    k             = tr[idx]->k[z];      /* k++ doesn't work; see above */
+	    inscount[k-1] = ESL_MAX(nins, inscount[k-1]); 
+	    nins          = 0;
 	    break;
 
 	  case p7T_T: /* T: record C-tail max, for a profile trace */
+	    inscount[M] = ESL_MAX(nins, inscount[M]);
+	    break;
+
 	  case p7T_E: /* this handles case of core traces, which do have I_M state  */
-	    inscount[M] = ESL_MAX(nins, inscount[M]); 
+	    inscount[k] = ESL_MAX(nins, inscount[k]); /* [M] doesn't work, because of {DMI}k->X->E frag exit */
 	    break;
 
 	  case p7T_B: /* B: record N-tail max for a profile trace; I0 for a core trace  */
@@ -302,7 +311,7 @@ map_new_msa(P7_TRACE **tr, int nseq, int M, int optflags, int **ret_inscount,
 	    nins = 0;
 	    break;
 
-	  case p7T_S: break;	/* don't need to do anything on S, X states */
+	  case p7T_S: break;	/* don't need to do anything on S,X states */
 	  case p7T_X: break;
 
 	  case p7T_J: p7_Die("J state unsupported");
@@ -351,9 +360,13 @@ get_dsq_z(ESL_SQ **sq, const ESL_MSA *premsa, P7_TRACE **tr, int idx, int z)
 }
 
 /* make_digital_msa()
- * Create a new digital MSA, given traces <tr> for digital <sq> or for a digital <premsa>.
- * (One and only one of <sq>,<premsa> are non-<NULL>.
+ * Create a new digital MSA, given traces <tr> for digital <sq> or for
+ * a digital <premsa>.  (One and only one of <sq>,<premsa> are
+ * non-<NULL>.
+ * The traces may either be profile traces or core traces;
+ * core traces may contain X "states" for fragments.
  */
+
 static int
 make_digital_msa(ESL_SQ **sq, const ESL_MSA *premsa, P7_TRACE **tr, int nseq, const int *matuse, const int *matmap, int M, int alen, int optflags, ESL_MSA **ret_msa)
 {
@@ -380,7 +393,7 @@ make_digital_msa(ESL_SQ **sq, const ESL_MSA *premsa, P7_TRACE **tr, int nseq, co
 	    msa->ax[idx][matmap[tr[idx]->k[z]]] = get_dsq_z(sq, premsa, tr, idx, z);
 	    apos = matmap[tr[idx]->k[z]] + 1;
 	    break;
-	    /* fallthru */
+
 	  case p7T_D:
 	    apos = matmap[tr[idx]->k[z]] + 1;
 	    break;
@@ -402,6 +415,10 @@ make_digital_msa(ESL_SQ **sq, const ESL_MSA *premsa, P7_TRACE **tr, int nseq, co
 	    apos = matmap[M]+1;	/* set position for C-terminal tail */
 	    break;
 	    
+	  case p7T_X:  /* peek at next state for B->X->Ik; superfluous for ->{DM}k; harmless for {MDI}k->X->E  */
+	    apos = matmap[tr[idx]->k[z+1]] + 1; /* this works because all other cases except B->X->Ik set their own apos */
+	    break;
+
 	  default:
 	    break;
 	  }
@@ -421,13 +438,15 @@ make_digital_msa(ESL_SQ **sq, const ESL_MSA *premsa, P7_TRACE **tr, int nseq, co
 
   
 /* make_text_msa()
- * Create a new digital MSA, given traces <tr> for digital <sq> or for a digital <premsa>.
+ * Create a new text MSA, given traces <tr> for digital <sq> or for a digital <premsa>.
  * (One and only one of <sq>,<premsa> are non-<NULL>.
  * 
  * The reason to make a text-mode MSA rather than let Easel handle printing a digital
  * MSA is to impose HMMER's standard representation on gap characters and insertions:
  * at inserts, gaps are '.' and residues are lower-case, whereas at matches, gaps are '-'
  * and residues are upper case.
+ *
+ * Also see comments in make_digital_msa(), above.
  */
 static int
 make_text_msa(ESL_SQ **sq, const ESL_MSA *premsa, P7_TRACE **tr, int nseq, const int *matuse, const int *matmap, int M, int alen, int optflags, ESL_MSA **ret_msa)
@@ -454,7 +473,9 @@ make_text_msa(ESL_SQ **sq, const ESL_MSA *premsa, P7_TRACE **tr, int nseq, const
 	  switch (tr[idx]->st[z]) {
 	  case p7T_M:
 	    msa->aseq[idx][-1+matmap[tr[idx]->k[z]]] = toupper(abc->sym[get_dsq_z(sq, premsa, tr, idx, z)]);
-	    /*fallthru*/
+	    apos = matmap[tr[idx]->k[z]]; /* i.e. one past the match column. remember, text mode is 0..alen-1 */
+	    break;
+
 	  case p7T_D:
 	    apos = matmap[tr[idx]->k[z]];
 	    break;
@@ -474,6 +495,10 @@ make_text_msa(ESL_SQ **sq, const ESL_MSA *premsa, P7_TRACE **tr, int nseq, const
 	    
 	  case p7T_E:
 	    apos = matmap[M];	/* set position for C-terminal tail */
+	    break;
+
+	  case p7T_X: /* see comments in make_digital_msa(); handling of B->X entry and X->E exit is tricky */
+	    apos = matmap[tr[idx]->k[z+1]]; 
 	    break;
 
 	  default:

documentation/alphaguide/glossary.tex
@@ -117,12 +117,15 @@
   sense). However, because a single structural domain may be detected
   in two or more pieces, the correspondence is not necessarily one to
   one; HMMER may call two or more ``domains'' in its output that
-  correspond to a single domain. Sorry, was that confusing? we
+  correspond to a single domain. Sorry, was that confusing? We
   probably should have used a name other than ``domain''.
 
 \item[\textbf{domain post-processing}] The steps that the pipeline
   goes through to identify individual domains, once it has decided
-  that a 
+  that a comparison is probably above reporting threshold. 
+  A Forward/Backward calculation is used to identify the probability
+  distribution over domain endpoints on the target sequence.
+
 
 \item[\textbf{envelope}]
 

src/Makefile.in
@@ -119,6 +119,7 @@ UTESTS =\
 	p7_trace_utest
 
 EXAMPLES = \
+	build_example\
 	generic_fwdback_example\
 	generic_msv_example\
 	generic_optacc_example\

commit f6b01ba536fca9338348444f7f4392b4e4e7ab5e

Bug #h77 fix: hmmalign corrupts column before an all-delete column.
Bug was introduced in r3041 (12 Nov 09) in fixing a problem with
hmmbuild resaved alignments, dealing with ->X-> fragment transitions,
reported by Elena. Affects 3.0rc1 only; not 3.0b3. 

Fix is in tracealign.c::make_{digital,text}_msa: test for matuse[k]
before writing a delete '-'.

i12-delete-corruption.pl added to test suite; tests for this bug.

Other small edits:
- LICENSE.sh.in brought over from release branch;
- esl_msa_MinimGaps prototype has changed, fixed in create-profmark.c



git-svn-id: https://svn.janelia.org/eddylab/eddys/src/hmmer/trunk@3196 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

bugs/BUGTRAX
@@ -1,3 +1,24 @@
+ID              h77
+TITLE           hmmalign corrupts column before an all-deleted column
+AFFECTS         3.0rc1
+FIXED_IN        -
+STATUS          CLOSED
+XREF            SRE J6/8; SRE 2010/0302-hmmer-bug-h77
+REPORTED_BY     Denis Schmidt  <Denis.Schmidt@merck.de>
+OPENED_DATE     SRE, Tue Mar  2 11:06:49 2010
+CLOSED_DATE     SRE, Tue Mar  2 11:47:25 2010
+DESCRIPTION     
+   hmmalign corrupts sequences, overwriting column with -'s if an
+   all-delete column occurs.
+
+   Bug was introduced in svn r3041, in fixing problems handling 
+   X states, detected by Elena.
+
+   Fix: in tracealign.c::make_{digital,text}_msa, test for matuse[k]
+   before writing a delete '-'. 
+  
+   i12-delete-corruption.pl in testsuite tests for this bug.
+//
 ####
 ####  3.0rc1 release: 9 Feb 2010
 ####

configure.ac
@@ -608,7 +608,7 @@ AC_SYS_LARGEFILE
 # Config subdirs and files (except main Makefile, which we defer)
 #
 if test -d release-notes; then
-  AC_CONFIG_FILES(release-notes/LICENSE.csh)
+  AC_CONFIG_FILES(release-notes/LICENSE.sh)
 fi
 if test -d documentation/userguide; then  
   AC_CONFIG_FILES([documentation/userguide/Makefile])

profmark/create-profmark.c
@@ -235,7 +235,7 @@ main(int argc, char **argv)
 	  esl_stack_Shuffle(cfg.r, teststack);
 	  synthesize_positives(go, &cfg, msa->name, teststack, &ntest);
 
-	  esl_msa_MinimGaps(trainmsa, NULL, NULL);
+	  esl_msa_MinimGaps(trainmsa, NULL, NULL, FALSE);
 	  esl_msa_Write(cfg.out_msafp, trainmsa, eslMSAFILE_STOCKHOLM);
 
 	  esl_dst_XAverageId(cfg.abc, trainmsa->ax, trainmsa->nseq, 10000, &avgid); /* 10000 is max_comparisons, before sampling kicks in */

release-notes/00CHECKLIST
@@ -131,7 +131,6 @@ Write the release notes in releases/.
 Check that the user guide is up to date.
 Revise the tutorial section. See last ~/notebook entry for scripts.
 Don't slack off on this step - it's a useful regression test as well.
-  
 
 Finally,
   make distclean
@@ -146,9 +145,8 @@ from the release directory
 
 ${HMMER_VERSION} tag used below should match @HMMER_VERSION@ in configure.ac, Makefile
 
-
 * You can put it in the environment and everything will work as cut/paste:
-   setenv HMMER_VERSION  3.0b3
+   setenv HMMER_VERSION  3.0rc1
    cd ~/releases
 
 * Delete any old releases of the same number
@@ -164,11 +162,11 @@ ${HMMER_VERSION} tag used below should match @HMMER_VERSION@ in configure.ac, Ma
    svn export https://svn.janelia.org/eddylab/eddys/src/hmmer/branches/3.0 hmmer-${HMMER_VERSION}
    cd hmmer-${HMMER_VERSION}
 
-  3.0b3: exported 3051/442
+  3.0rc1: exported 3160/510
+  3.0b3:  exported 3051/442
 
 * Edit configure.ac: make sure version, date, copyright info is set correctly
    emacs configure.ac
-   ln -s easel/aclocal.m4 .
    autoconf; (cd easel; autoconf)
    ./configure
    source release-notes/LICENSE.csh
@@ -192,65 +190,45 @@ ${HMMER_VERSION} tag used below should match @HMMER_VERSION@ in configure.ac, Ma
    easel/devkit/sedition HMMER_VERSION ${HMMER_VERSION} HMMER_DATE "${HMMER_DATE}" HMMER_COPYRIGHT "${HMMER_COPYRIGHT}" release-notes/LICENSE.tag
    find .     -type f -exec easel/devkit/sedition-pp LICENSE release-notes/LICENSE.tag {} \;
 
+
 * compile documentation
-  Beta Guide:
-   cd documentation/alphaguide
+   cd documentation/userguide
    make
    mv Userguide.pdf ../../Userguide.pdf
    cd ../..
 
-  User Guide (not ready yet)
-   cd documentation/userguide
-   ln -s plan7-core.ai plan7-core.pdf
-   ln -s plan7-search.ai  plan7-search.pdf
-   make
-   pdflatex main
-   mv Userguide.pdf ../../
-   cd ../..	 
-
-* In Makefile.in, comment out Makefile and configure targets - 
-  users may not have autoconf installed.
-
 
 * compile Linux binaries with icc
   Each build can run in a separate terminal; we're using independent build directories
+  My .bashrc contains two functions for configuring icc paths properly:
+   setup_icc_intel64
+   setup_icc_ia32
 
+x86-64 (intel64) build:
    ssh login-eddy
-   setenv HMMER_VERSION  3.0b3
-   source /usr/local/intel/current/bin/iccvars.csh intel64
+   export HMMER_VERSION=3.0rc1
    cd releases/hmmer-${HMMER_VERSION}
-   source release-notes/LICENSE.csh
+   setup_icc_intel64
    mkdir build-x86_64
    cd    build-x86_64
    ../configure CC=icc LDFLAGS=-static-intel  --bindir=${HOME}/releases/hmmer-${HMMER_VERSION}/binaries/intel-linux-x86_64
    make
-   make check
    make dev
+   make check
    make install
    (cd easel/miniapps; make install)
 
- Building ia32 is a little tricky, because our environment is so thoroughly x86_64.
- First go to .cshrc and comment out the
-   source /usr/local/INTEL.csh line
- Then:
-
+ia32 build:
    ssh login-eddy
-   setenv HMMER_VERSION  3.0b3
-   source /usr/local/intel/ictce/current/ictce/3.2.0.020/ictvars.csh
-   source /usr/local/intel/ictce/current/cc/11.0.074/bin/ia32/idbvars.csh
-   source /usr/local/intel/ictce/current/cc/11.0.074/bin/iccvars.csh ia32
-   source /usr/local/intel/ictce/current/fc/11.0.074/bin/ifortvars.csh ia32
-   source /usr/local/intel/ictce/current/itac/7.2.0.011/bin/itacvars.csh
-   source /usr/local/intel/ictce/current/impi/3.2.0.011/bin64/mpivars.csh
-   setenv INTEL_LICENSE_FILE 28518@b03u25.int.janelia.org
+   export HMMER_VERSION=3.0rc1
    cd releases/hmmer-${HMMER_VERSION}
-   source release-notes/LICENSE.csh
+   setup_icc_ia32
    mkdir build-ia32
    cd    build-ia32
    ../configure CC=icc LDFLAGS=-static-intel --bindir=${HOME}/releases/hmmer-${HMMER_VERSION}/binaries/intel-linux-ia32
    make
-   make check
    make dev
+   make check
    make install
    (cd easel/miniapps; make install)
 
@@ -259,14 +237,13 @@ ${HMMER_VERSION} tag used below should match @HMMER_VERSION@ in configure.ac, Ma
 
    make distclean
    cp   release-notes/RELEASE-${HMMER_VERSION} RELEASE-NOTES
+   rm -rf bugs
    rm -rf build-ia32   
-   rm -rf build-ia32-debug
    rm -rf build-x86_64
    rm -rf contrib
+   rm -rf documentation/userguide
    rm -rf release-notes
-   rm -rf bugs
-
-   rm -rf documentation
+   rm -rf test-speed
 
    cd easel
    rm -f  00CHECKLIST LICENSE.csh.in LICENSE.tag 
@@ -277,7 +254,7 @@ ${HMMER_VERSION} tag used below should match @HMMER_VERSION@ in configure.ac, Ma
 
    cat RELEASE-NOTES | dumpnotes
    emacs RELEASE-NOTES
-   emacs ~/src/hmmer/release-notes/RELEASE-3.0b2
+Delete TO-DO sections and clean up for public consumption.
    texclean
 
 * package
@@ -286,7 +263,8 @@ ${HMMER_VERSION} tag used below should match @HMMER_VERSION@ in configure.ac, Ma
    tar cvf hmmer-${HMMER_VERSION}.tar hmmer-${HMMER_VERSION}
    gzip hmmer-${HMMER_VERSION}.tar
 
-   3.0b3: 17.1 Mb
+   3.0rc1: 21.0 Mb
+   3.0b3:  17.1 Mb
 
 * test again
    cd hmmer-${HMMER_VERSION}
@@ -308,20 +286,30 @@ ${HMMER_VERSION} tag used below should match @HMMER_VERSION@ in configure.ac, Ma
    cd ~/releases/hmmer-release
 
    svn copy https://svn.janelia.org/eddylab/eddys/src/hmmer/branches/3.0\
-            https://svn.janelia.org/eddylab/eddys/src/hmmer/tags/3.0b3\
-            -m "tagged 3.0b3 release, from 3.0 branch"          
+            https://svn.janelia.org/eddylab/eddys/src/hmmer/tags/3.0rc1\
+            -m "tagged 3.0rc1 release, from 3.0 branch"          
    svn copy https://svn.janelia.org/eddylab/eddys/easel/branches/hmmer/3.0\
-            https://svn.janelia.org/eddylab/eddys/easel/tags/hmmer/3.0b3\
-            -m "tagged Easel for HMMER 3.0b3 release, from HMMER 3.0 branch"          
+            https://svn.janelia.org/eddylab/eddys/easel/tags/hmmer/3.0rc1\
+            -m "tagged Easel for HMMER 3.0rc1 release, from HMMER 3.0 branch"          
 
 * Merge the branch's changes back to the trunk
+  You must do this by diff of specific revisions in the branch.
+  Do *not* diff the branch against the trunk; if the trunk has its own
+  changes that should be retained, you'll revert them to the branch
+  version!
 
-  HMMER: -r 3050:3051
-  Easel: no changes
+  HMMER: -r 3152:3161
+  Easel: -r 509:511
 
   cd ~/src/hmmer
+  svn diff  -r 3152:3161 https://svn.janelia.org/eddylab/eddys/src/hmmer/branches/3.0 | more
+  cd easel
+  svn diff -r 509:511    https://svn.janelia.org/eddylab/eddys/easel/branches/hmmer/3.0 | more
+  cd ..
+No changes in Easel 
+
   svn update
-  svn merge -r 3050:3051 https://svn.janelia.org/eddylab/eddys/src/hmmer/branches/3.0
+  svn merge -r 3152:3161 https://svn.janelia.org/eddylab/eddys/src/hmmer/branches/3.0
   
   This screws up the Easel externals on the trunk, so fix:
  
@@ -349,3 +337,4 @@ Release           Date            SVN (HMMER/Easel)
 3.0b1             17 Jun 2009     r2831/r345
 3.0b2             21 Jun 2009     r2837/r350
 3.0b3             15 Nov 2009     r3051/r442
+3.0rc1            10 Feb 2010     r3161/r511

release-notes/LICENSE.csh.in
@@ -1,10 +1,0 @@
-setenv RELEASE         @HMMER_VERSION@
-setenv HMMER_VERSION   @HMMER_VERSION@
-setenv HMMER_DATE      "@HMMER_DATE@"
-setenv HMMER_COPYRIGHT "@HMMER_COPYRIGHT@"
-setenv HMMER_LICENSE   "@HMMER_LICENSE@"
-setenv EASEL_VERSION   @EASEL_VERSION@ 
-setenv EASEL_DATE      "@EASEL_DATE@"
-setenv EASEL_COPYRIGHT "@EASEL_COPYRIGHT@"
-setenv EASEL_LICENSE   "@EASEL_LICENSE@"
-

release-notes/LICENSE.sh.in
@@ -1,0 +1,10 @@
+export RELEASE=@HMMER_VERSION@
+export HMMER_VERSION=@HMMER_VERSION@
+export HMMER_DATE="@HMMER_DATE@"
+export HMMER_COPYRIGHT="@HMMER_COPYRIGHT@"
+export HMMER_LICENSE="@HMMER_LICENSE@"
+export EASEL_VERSION=@EASEL_VERSION@ 
+export EASEL_DATE="@EASEL_DATE@"
+export EASEL_COPYRIGHT="@EASEL_COPYRIGHT@"
+export EASEL_LICENSE="@EASEL_LICENSE@"
+

src/tracealign.c
@@ -403,7 +403,8 @@ make_digital_msa(ESL_SQ **sq, const ESL_MSA *premsa, P7_TRACE **tr, int nseq, co
 	    break;
 
 	  case p7T_D:
-	    msa->ax[idx][matmap[tr[idx]->k[z]]] = esl_abc_XGetGap(abc); /* overwrites ~ in Dk column on X->Dk */
+	    if (matuse[tr[idx]->k[z]]) /* bug h77: if all col is deletes, do nothing; do NOT overwrite a column */
+	      msa->ax[idx][matmap[tr[idx]->k[z]]] = esl_abc_XGetGap(abc); /* overwrites ~ in Dk column on X->Dk */
 	    apos = matmap[tr[idx]->k[z]] + 1;
 	    break;
 
@@ -505,7 +506,8 @@ make_text_msa(ESL_SQ **sq, const ESL_MSA *premsa, P7_TRACE **tr, int nseq, const
 	    break;
 
 	  case p7T_D:
-	    msa->aseq[idx][-1+matmap[tr[idx]->k[z]]] = '-';  /* overwrites ~ in Dk column on X->Dk */
+	    if (matuse[tr[idx]->k[z]]) /* bug #h77: if all column is deletes, do nothing; do NOT overwrite a column */
+	      msa->aseq[idx][-1+matmap[tr[idx]->k[z]]] = '-';  /* overwrites ~ in Dk column on X->Dk */
 	    apos = matmap[tr[idx]->k[z]];
 	    break;
 

testsuite/i12-delete-corruption.pl
@@ -1,0 +1,65 @@
+#! /usr/bin/perl
+
+# Bug #h77: hmmalign corrupts column preceding an all-delete column
+#
+# Usage:   ./i12-delete-corruption.pl <builddir> <srcdir> <tmpfile prefix>
+# Example: ./i12-delete-corruption.pl ..         ..       tmpfoo
+#
+# SRE, Tue Mar  2 11:11:07 2010 [Janelia]
+# SVN $Id$
+
+
+BEGIN {
+    $builddir  = shift;
+    $srcdir    = shift;
+    $tmppfx    = shift;
+}
+
+# Verify that we have all the executables we need for the test.
+if (! -x "$builddir/src/hmmbuild")   { die "FAIL: didn't find hmmbuild binary in $builddir/src\n";  }
+if (! -x "$builddir/src/hmmalign")   { die "FAIL: didn't find hmmalign binary in $builddir/src\n";  }
+
+# Create our test files.
+# The test seqs are missing one column, K, in an unambiguous small alignment.
+if (! open(ALI1, ">$tmppfx.sto")) { die "FAIL: couldn't open $tmppfx.sto for write\n";  }
+if (! open(SEQ1, ">$tmppfx.fa"))  { die "FAIL: couldn't open $tmppfx.fa for write\n";  }
+
+print ALI1 <<"EOF";
+# STOCKHOLM 1.0
+seq1 ACDEFGHIKLMNPQRSTVWY
+seq2 ACDEFGHIKLMNPQRSTVWY
+seq3 ACDEFGHIKLMNPQRSTVWY
+//
+EOF
+
+print SEQ1 <<"EOF";
+>test1
+ACDEFGHILMNPQRSTVWY
+>test2
+ACDEFGHILMNPQRSTVWY
+EOF
+
+close ALI1;
+close SEQ1;
+
+@output = `$builddir/src/hmmbuild $tmppfx.hmm $tmppfx.sto 2>&1`;
+if ($? != 0) { die "FAIL: hmmbuild failed\n"; }
+
+$output = `$builddir/src/hmmalign $tmppfx.hmm $tmppfx.fa 2>&1`;
+if ($? != 0) { die "FAIL: hmmalign failed\n"; }
+
+($testseq) = ($output =~ /\ntest1\s+(\S+)/);
+if ($testseq ne "ACDEFGHILMNPQRSTVWY") { die "FAIL: test sequence corrupted by hmmalign\n"; }
+
+$output = `$builddir/src/hmmalign --allcol $tmppfx.hmm $tmppfx.fa 2>&1`;
+if ($? != 0) { die "FAIL: hmmalign failed\n"; }
+
+($testseq) = ($output =~ /\ntest1\s+(\S+)/);
+if ($testseq ne "ACDEFGHI-LMNPQRSTVWY") { die "FAIL: hmmalign --allcol produced unexpected result\n"; }
+
+print "ok\n";
+unlink "$tmppfx.sto";
+unlink "$tmppfx.fa";
+unlink "$tmppfx.hmm";
+exit 0;
+

testsuite/testsuite.sqc
@@ -121,6 +121,7 @@
 1 exercise  opt-annotation        !testsuite/i9-optional-annotation.pl! @@ !! %OUTFILES%
 1 exercise  dup-names             !testsuite/i10-duplicate-names.pl!    @@ !! %OUTFILES%
 1 exercise  mapali-again          !testsuite/i11-hmmalign-mapali.pl!    @@ !! %OUTFILES%
+1 exercise  delete-corruption     !testsuite/i12-delete-corruption.pl!  @@ !! %OUTFILES%
 
 1 exercise  brute-itest           @src/itest_brute@  
 1 exercise  hmmpress-itest        !src/hmmpress.itest.pl! @src/hmmpress@ %MINIFAM.HMM% %TMPPFX%