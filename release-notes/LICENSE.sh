d1352b322bdd03e98b7f6b8d008391329cec81d8
Sean Eddy <sean@eddylab.org>
2010-03-08 16:19:31 +0000

    3.0rc2 preparation. Tutorial and other documentation updated. Release notes written.
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/hmmer/branches/3.0@3224 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

---
+++ release-notes/LICENSE.sh
@@ -0,0 +1,10 @@
+export RELEASE=3.0rc2
+export HMMER_VERSION=3.0rc2
+export HMMER_DATE="March 2010"
+export HMMER_COPYRIGHT="Copyright (C) 2010 Howard Hughes Medical Institute."
+export HMMER_LICENSE="Freely distributed under the GNU General Public License (GPLv3)."
+export EASEL_VERSION=h3.0rc2 
+export EASEL_DATE="March 2010"
+export EASEL_COPYRIGHT="Copyright (C) 2010 Howard Hughes Medical Institute."
+export EASEL_LICENSE="Freely distributed under the Janelia Farm Software License."
+
