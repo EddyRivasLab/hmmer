7d22955a124b30d137aa5f569a6d85f2d5e64872
Sean Eddy <sean@eddylab.org>
2011-01-14 22:16:46 +0000

    Added support for nightly automated builds, in the "autobuild" directory. See 00README there.
    Four build config scripts to start with: intel-linux-gcc, for example.
    
    One script for building a single config and automatically logging
    problems: autobuild.pl.
    
    One script specifying the entire nightly build process, w/ a table of
    all build configs and compile hosts: hmmer_autobuilds.pl. 
    
    Scheduled a cron job for 3:31am every night on my desktop machine to
    run hmmer_autobuilds.pl and email results table to me.
    
    Assuming this works tonight, we can add more build configs and
    exercise all of that lovely compile farm, every night. Never again
    will someone break portability (oh, VMX/Altivec for example) and have
    it go unnoticed for months.
    
    No reason why the system wouldn't work for Easel or Infernal, but
    let's see if it works in H3 for now.
    
    
    
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/hmmer/trunk@3473 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

---
+++ autobuild/hmmer_autobuilds.pl
@@ -0,0 +1,42 @@
+#! /usr/bin/perl
+
+# Nightly builds for HMMER3
+# 
+# Usage:     hmmer_autobuilds.pl  <srcdir>
+# Example:   hmmer_autobuilds.pl  ~/nightlies/hmmer/trunk > /tmp/hmmer_autobuilds.log
+#
+# SVN $URL$
+# SVN $Id$
+# SRE, Fri Jan 14 16:02:36 2011 [Janelia]
+
+
+@buildconfigs = (
+    { name => "intel-linux-icc-mpi",      host => "login-eddy"  },
+    { name => "intel-linux-gcc",          host => "login-eddy"  },
+    { name => "intel-macosx-gcc-debug",   host => "."           },
+    { name => "intel-macosx-gcc",         host => "."           },
+    );
+
+if ($#ARGV+1 != 1) { die "FAIL: incorrect number of command line arguments"; }
+$srcdir = shift;
+if (! -d $srcdir) { die "FAIL: source working directory $srcdir not found"; }
+
+# First we update in the source working directory.
+#
+chdir $srcdir ||    die "FAIL: couldn't cd to $srcdir"; 
+system("svn update > autobuilds.log 2>&1");             if ($?) { die "FAIL: svn update"; }
+system("autoconf   > autobuilds.log 2>&1 ");            if ($?) { die "FAIL: H3 autoconf"; }
+system("(cd easel; autoconf) > autobuilds.log 2>&1");   if ($?) { die "FAIL: esl autoconf"; }
+
+# Then we try to build on everything
+#
+foreach $build (@buildconfigs)
+{
+    $script = $build->{name};
+    $cmd = "$srcdir/autobuild/autobuild.pl $srcdir $script";
+    if ($build->{host} ne ".") { $cmd = "ssh $build->{host} \"$cmd\""; }
+
+    printf ("%-30s %-20s ", $build->{name}, $build->{host});
+    $output = `$cmd`;
+    print $output;
+}
