EddyRivasLab/hmmer

commit c2407f7b06f1876756ed879f6ab2ac65543f1b4c

Bugfix #h90: phmmer consensus line differs from query sequence.  This bugfix required changes to save file formats and the P7_HMM data structure, and thus may affect other code.
Description of bug #h90: In displayed alignments, the model consensus
line was constructed by choosing maximum likelihood residues at each
position. With single sequence queries, this can produce a
counterintuitive effect, because the most likely residue is not
necessarily the original residue. For example, under BLOSUM62 scoring,
the most likely residue to align to an M is an L, not another M.

Consensus of a model is now set in the model itself, at model
construction time. (Previously, consensus was set in the profile, at
model->profile configuration time.) This allows single-sequence models
to set consensus specially.

HMM save file format upgraded to 3/e.  Models now include a consensus
residue annotation. Each match state in a save file is followed by
four annotations <map> <cons> <rf> <cs>; before it was just three,
<map> <rf> <cs>. Header includes a CONS <yes|no> line indicating when
consensus residues are present. This change will affect any program
trying to write its own models in HMMER3 formats. (They can continue
to write formats <= 3/d; HMMER3 parsers are backwards compatible.)
p7_hmmfile.c parsers upgraded to 3/e format; examples HMMs in tutorial
and tetssuite upgraded; documentation upgraded.

P7_HMM structure now includes hmm->consensus field. Previously,
consensus was only in profiles (P7_PROFILE, P7_OPROFILE). 

p7_hmm_SetConsensus() determines the consensus residue line. It treats
single sequence queries and MSA queries as separate cases.

p7_ProfileConfig() requires that the HMM has consensus residue
annotation, so it can transfer that annotation to the profile.
It no longer calculates consensus itself.

p7_Builder() and p7_SingleBuilder() call p7_hmm_SetConsensus() as one
of their construction steps.

removed: p7_hmm_Rescale(). (it was redundant with p7_hmm_Scale().)

src/hmmdwrkr.c
@@ -380,51 +380,24 @@ process_QueryCmd(HMMD_COMMAND *cmd, WORKER_ENV *env)
 
     /* fill in the hmm pointers */
     n = sizeof(float) * (hmm->M + 1) * p7H_NTRANSITIONS;
-    memcpy(*hmm->t, p, n);
-    p += n;
+    memcpy(*hmm->t, p, n);     p += n;
 
     n = sizeof(float) * (hmm->M + 1) * query->abc->K;
-    memcpy(*hmm->mat, p, n);
-    p += n;
-    memcpy(*hmm->ins, p, n);
-    p += n;
+    memcpy(*hmm->mat, p, n);   p += n;
+    memcpy(*hmm->ins, p, n);   p += n;
 
-    if (thmm.name != NULL) {
-      hmm->name = strdup(p);
-      p += strlen(hmm->name) + 1;
-    }
-
-    if (thmm.acc != NULL) {
-      hmm->acc = strdup(p);
-      p += strlen(hmm->name) + 1;
-    }
-
-    if (thmm.desc != NULL) {
-      hmm->desc = strdup(p);
-      p += strlen(hmm->name) + 1;
-    }
+    if (thmm.name) { hmm->name = strdup(p); p += strlen(hmm->name) + 1; }
+    if (thmm.acc)  { hmm->acc  = strdup(p); p += strlen(hmm->name) + 1; }
+    if (thmm.desc) { hmm->desc = strdup(p); p += strlen(hmm->name) + 1; }
 
     n = hmm->M + 2;
-    if (hmm->flags & p7H_RF) {
-      memcpy(hmm->rf, p, n);
-      p += n;
-    }
+    if (hmm->flags & p7H_RF)   { memcpy(hmm->rf,        p, n); p += n; }
+    if (hmm->flags & p7H_CONS) { memcpy(hmm->consensus, p, n); p += n; }
+    if (hmm->flags & p7H_CS)   { memcpy(hmm->cs,        p, n); p += n; }
+    if (hmm->flags & p7H_CA)   { memcpy(hmm->ca,        p, n); p += n; }
 
-    if (hmm->flags & p7H_CS) {
-      memcpy(hmm->cs, p, n);
-      p += n;
-    }
-
-    if (hmm->flags & p7H_CA) {
-      memcpy(hmm->ca, p, n);
-      p += n;
-    }
-
-    if (hmm->flags & p7H_MAP) {
-      n = sizeof(int) * (hmm->M + 1);
-      memcpy(hmm->map, p, n);
-      p += n;
-    }
+    n = sizeof(int) * (hmm->M + 1);
+    if (hmm->flags & p7H_MAP) {  memcpy(hmm->map,       p, n); p += n; }
 
     query->hmm = hmm;
   }

bugs/BUGTRAX
@@ -1,3 +1,29 @@
+ID              h90
+TITLE           phmmer's displayed consensus differs from query sequence
+AFFECTS         3.0
+FIXED_IN        -
+STATUS          CLOSED
+XREF            SRE:J8/26
+REPORTED_BY     Rob Finn
+OPENED_DATE     SRE, Thu Jun 16 11:20:29 2011
+CLOSED_DATE     SRE, Thu Jun 16 11:20:29 2011
+DESCRIPTION     
+  In displayed alignments, the model consensus line was constructed by
+  choosing maximum likelihood residues at each position. With single
+  sequence queries, this can produce a counterintuitive effect, because
+  the most likely residue is not necessarily the original residue. For
+  example, under BLOSUM62 scoring, the most likely residue to align to
+  an M is an L, not another M.
+
+  Consensus of a model is now set in the model itself, at model
+  construction time. (Previously, consensus was set in the profile, at
+  model->profile configuration time.) This allows single-sequence
+  models to set consensus specially.
+
+  Implemented p7_hmm_SetConsensus(), which handles single sequences
+  and standard multiple-alignment models as different cases. Added
+  <consensus> field to the <P7_HMM> structure.
+//
 ID              h89
 TITLE           HMMER_NCPU env var sets --cpu, creates incompatibility with -n, --mpi
 AFFECTS         3.0

documentation/userguide/formats.tex
@@ -10,53 +10,56 @@ save file. An abridged version is shown here, where (\ldots) mark
 deletions made for clarity and space:
 
 \begin{tinysreoutput}
-HMMER3/b [3.0b2 | June 2009]
+HMMER3/e [3.0 | March 2010]
 NAME  fn3
-ACC   PF00041.12
+ACC   PF00041.13
 DESC  Fibronectin type III domain
 LENG  86
 ALPH  amino
 RF    no
+CONS  yes
 CS    yes
 MAP   yes
-DATE  Mon Jun 15 09:39:41 2009
+DATE  Thu Jun 16 11:48:22 2011
 NSEQ  106
 EFFN  11.415833
-CKSUM 72638555
+CKSUM 3564431818
 GA    8.00 7.20
 TC    8.00 7.20
 NC    7.90 7.90
 STATS LOCAL MSV       -9.4043  0.71847
 STATS LOCAL VITERBI   -9.7737  0.71847
 STATS LOCAL FORWARD   -3.8341  0.71847
-HMM          A        C        D        E        F        G        H        I     (...)    Y   
+HMM          A        C        D        E        F        G        H        I    (...)    Y   
             m->m     m->i     m->d     i->m     i->i     d->m     d->d
-  COMPO   2.70271  4.89246  3.02314  2.64362  3.59817  2.82566  3.74147  3.08574  (...) 3.22607
-          2.68618  4.42225  2.77519  2.73123  3.46354  2.40513  3.72494  3.29354  (...) 3.61503
+  COMPO   2.70330  4.91262  3.03272  2.64079  3.60307  2.84344  3.74204  3.07942 (...) 3.21526
+          2.68618  4.42225  2.77519  2.73123  3.46354  2.40513  3.72494  3.29354 (...) 3.61503
           0.00338  6.08833  6.81068  0.61958  0.77255  0.00000        *
-      1   3.16986  5.21447  4.52134  3.29953  4.34285  4.18764  4.30886  3.35801  (...) 3.93889      1 - -
-          2.68629  4.42236  2.77530  2.73088  3.46365  2.40512  3.72505  3.29365  (...) 3.61514
+      1   3.16986  5.21447  4.52134  3.29953  4.34285  4.18764  4.30886  3.35801 (...) 3.93889      1 p - -
+          2.68629  4.42236  2.77530  2.73088  3.46365  2.40512  3.72505  3.29365 (...) 3.61514
           0.09796  2.38361  6.81068  0.10064  2.34607  0.48576  0.95510
-      2   2.70230  5.97353  2.24744  2.62947  5.31433  2.60356  4.43584  4.79731  (...) 4.25623      3 - -
-          2.68618  4.42225  2.77519  2.73123  3.46354  2.40513  3.72494  3.29354  (...) 3.61503
+      2   2.70230  5.97353  2.24744  2.62947  5.31433  2.60356  4.43584  4.79731 (...) 4.25623      3 s - -
+          2.68618  4.42225  2.77519  2.73123  3.46354  2.40513  3.72494  3.29354 (...) 3.61503
           0.00338  6.08833  6.81068  0.61958  0.77255  0.48576  0.95510
 (...)
-     85   2.48488  5.72055  3.87501  1.97538  3.04853  3.48010  4.51877  3.51898  (...) 3.43366    120 - B
-          2.68618  4.42225  2.77519  2.73123  3.46354  2.40513  3.72494  3.29354  (...) 3.61503
+     85   2.48488  5.72055  3.87501  1.97538  3.04853  3.48010  4.51877  3.51898 (...) 3.43366    120 e - B
+          2.68618  4.42225  2.77519  2.73123  3.46354  2.40513  3.72494  3.29354 (...) 3.61503
           0.00338  6.08833  6.81068  0.61958  0.77255  0.48576  0.95510
-     86   3.03720  5.94099  3.75455  2.96917  5.26587  2.91682  3.66571  4.11840  (...) 4.99111    121 - E
-          2.68618  4.42225  2.77519  2.73123  3.46354  2.40513  3.72494  3.29354  (...) 3.61503
+     86   3.03720  5.94099  3.75455  2.96917  5.26587  2.91682  3.66571  4.11840 (...) 4.99111    121 s - E
+          2.68618  4.42225  2.77519  2.73123  3.46354  2.40513  3.72494  3.29354 (...) 3.61503
           0.00227  6.08723        *  0.61958  0.77255  0.00000        *
 //
 \end{tinysreoutput}
 
-An HMM file consists of one or more HMMs.  Each HMM starts with the
-identifier \prog{HMMER3/b} and ends with \prog{//} on a line by
-itself.  The identifier allows backward compatibility as the HMMER
-software evolves: it tells the parser this file is from HMMER3's save
-file format version b. (The 3/a format was used in alpha test versions
-of HMMER3.)  The closing \prog{//} allows multiple HMMs to be
-concatenated.
+An HMM file consists of one or more HMMs.  Each HMM starts with a
+format version identifier (here, \prog{HMMER3/e}) and ends with
+\prog{//} on a line by itself.  The format version identifier allows
+backward compatibility as the HMMER software evolves: it tells the
+parser this file is from HMMER3's save file format version
+e.\footnote{HMMER 3.0 used 3/b format. HMMER 3.1 uses 3/e format.
+  Some alpha test versions of 3.0 used 3/a format. Internal
+  development versions of 3.1 used 3/c and 3/d formats.}  The closing
+\prog{//} allows multiple HMMs to be concatenated.
 
 The format is divided into two regions. The first region contains
 textual information and miscalleneous parameters in a roughly
@@ -134,6 +137,18 @@ alignment file's \verb+#=GC RF+ line. It is propagated to alignment
 outputs, and also may optionally be used to define consensus match
 columns in profile HMM construction. \textbf{Optional}; assumed to be
 no if not present.
+
+\item [\emprog{CONS <s>}] Consensus residue annotation flag;
+  \prog{<s>} is either \prog{no} or \prog{yes} (case insensitive).  If
+  \prog{yes}, the consensus residue field for each match state in the
+  main model (see below) is valid. If \prog{no}, these characters are
+  ignored. Consensus residue annotation is determined when models are
+  built. For models of single sequences, the consensus is the same as
+  the query sequence. For models of multiple alignments, the consensus
+  is the maximum likelihood residue at each position. Upper case
+  indicates that the model's emission probability for the consensus
+  residue is $\geq$ an arbitrary threshold (0.5 for protein models,
+  0.9 for DNA/RNA models).
 
 \item [\emprog{CS <s>}] Consensus structure annotation flag;
 \prog{<s>} is either \prog{no} or \prog{yes} (case insensitive). If
@@ -263,6 +278,11 @@ The next field is the \prog{MAP} annotation for this node.  If
 \prog{MAP} was \prog{yes} in the header, then this is an integer,
 representing the alignment column index for this match state
 (1..alen); otherwise, this field is `-'.
+
+The next field is the \prog{CONS} consensus residue for this node.  If
+\prog{CONS} was \prog{yes} in the header, then this is a single
+character, representing the consensus residue annotation for this
+match state; otherwise, this field is `-'.
 
 The next field is the \prog{RF} annotation for this node.  If
 \prog{RF} was \prog{yes} in the header, then this is a single

src/generic_viterbi.c
@@ -304,6 +304,7 @@ utest_basic(ESL_GETOPTS *go)
   if (esl_msa_Digitize(abc, msa, NULL)             != eslOK) esl_fatal("failed to digitize MSA");
   if (p7_Fastmodelmaker(msa, 0.5, &hmm, NULL)      != eslOK) esl_fatal("failed to create GAATTC model");
   if (p7_ParameterEstimation(hmm, pri)             != eslOK) esl_fatal("failed to parameterize GAATTC model");
+  if (p7_hmm_SetConsensus(hmm, NULL)               != eslOK) esl_fatal("failed to make consensus");
   if ((bg = p7_bg_Create(abc))                     == NULL)  esl_fatal("failed to create DNA null model");
   if ((gm = p7_profile_Create(hmm->M, abc))        == NULL)  esl_fatal("failed to create GAATTC profile");
   if (p7_ProfileConfig(hmm, bg, gm, L, p7_UNILOCAL)!= eslOK) esl_fatal("failed to config profile");

src/h2_io.c
@@ -74,6 +74,7 @@ p7_h2io_WriteASCII(FILE *fp, P7_HMM *hmm)
   fprintf(fp, "RF    %s\n", (hmm->flags & p7H_RF)  ? "yes" : "no");
   fprintf(fp, "CS    %s\n", (hmm->flags & p7H_CS)  ? "yes" : "no");
   fprintf(fp, "MAP   %s\n", (hmm->flags & p7H_MAP) ? "yes" : "no");
+  /* H3 consensus line has no counterpart in H2 */
 
   if (hmm->comlog != NULL)  h2_multiline(fp, "COM   ",     hmm->comlog);
   if (hmm->nseq   != -1)    fprintf     (fp, "NSEQ  %d\n", hmm->nseq);

src/hmmdmstr.c
@@ -1414,13 +1414,14 @@ clientside_loop(CLIENTSIDE_ARGS *data)
     n = n + sizeof(float) * (hmm->M + 1) * p7H_NTRANSITIONS;
     n = n + sizeof(float) * (hmm->M + 1) * abc->K;
     n = n + sizeof(float) * (hmm->M + 1) * abc->K;
-    if (hmm->name   != NULL) n = n + strlen(hmm->name) + 1;
-    if (hmm->acc    != NULL) n = n + strlen(hmm->acc)  + 1;
-    if (hmm->desc   != NULL) n = n + strlen(hmm->desc) + 1;
-    if (hmm->flags & p7H_RF) n = n + hmm->M + 2;
-    if (hmm->flags & p7H_CS) n = n + hmm->M + 2;
-    if (hmm->flags & p7H_CA) n = n + hmm->M + 2;
-    if (hmm->flags & p7H_MAP) n = n + sizeof(int) * (hmm->M + 1);
+    if (hmm->name   != NULL)   n = n + strlen(hmm->name) + 1;
+    if (hmm->acc    != NULL)   n = n + strlen(hmm->acc)  + 1;
+    if (hmm->desc   != NULL)   n = n + strlen(hmm->desc) + 1;
+    if (hmm->flags & p7H_RF)   n = n + hmm->M + 2;
+    if (hmm->flags & p7H_CONS) n = n + hmm->M + 2;
+    if (hmm->flags & p7H_CS)   n = n + hmm->M + 2;
+    if (hmm->flags & p7H_CA)   n = n + hmm->M + 2;
+    if (hmm->flags & p7H_MAP)  n = n + sizeof(int) * (hmm->M + 1);
   }
 
   if ((cmd = malloc(n)) == NULL) LOG_FATAL_MSG("malloc", errno);
@@ -1473,9 +1474,10 @@ clientside_loop(CLIENTSIDE_ARGS *data)
     if (hmm->desc) { n = strlen(hmm->desc) + 1;  memcpy(ptr, hmm->desc, n);  ptr += n; }
 
     n = hmm->M + 2;
-    if (hmm->flags & p7H_RF)  { memcpy(ptr, hmm->rf, n); ptr += n; }
-    if (hmm->flags & p7H_CS)  { memcpy(ptr, hmm->cs, n); ptr += n; }
-    if (hmm->flags & p7H_CA)  { memcpy(ptr, hmm->ca, n); ptr += n; }
+    if (hmm->flags & p7H_RF)    { memcpy(ptr, hmm->rf,        n); ptr += n; }
+    if (hmm->flags & p7H_CONS)  { memcpy(ptr, hmm->consensus, n); ptr += n; }
+    if (hmm->flags & p7H_CS)    { memcpy(ptr, hmm->cs,        n); ptr += n; }
+    if (hmm->flags & p7H_CA)    { memcpy(ptr, hmm->ca,        n); ptr += n; }
 
     if (hmm->flags & p7H_MAP) {
       n = sizeof(int) * (hmm->M + 1);

src/hmmer.h
@@ -116,7 +116,7 @@ enum p7_offsets_e  { p7_MOFFSET = 0, p7_FOFFSET = 1, p7_POFFSET = 2 };
 #define p7H_CA      (1<<13)   /* surface accessibilities available               !*/
 #define p7H_COMPO   (1<<14)   /* model-specific residue composition available     */
 #define p7H_CHKSUM  (1<<15)   /* model has an alignment checksum                  */
-
+#define p7H_CONS    (1<<16)   /* consensus residue line available                 */
 
 /* Indices of Plan7 main model state transitions, hmm->t[k][] */
 enum p7h_transitions_e {
@@ -166,8 +166,10 @@ typedef struct p7_hmm_s {
   char    *acc;	                 /* accession number of model (Pfam)      (optional: NULL) */ /* String, \0-terminated   */
   char    *desc;                 /* brief (1-line) description of model   (optional: NULL) */ /* String, \0-terminated   */
   char    *rf;                   /* reference line from alignment 1..M    (p7H_RF)         */ /* String; 0=' ', M+1='\0' */
+  char    *consensus;		 /* consensus residue line        1..M    (p7H_CONS)       */ /* String; 0=' ', M+1='\0' */
   char    *cs;                   /* consensus structure line      1..M    (p7H_CS)         */ /* String; 0=' ', M+1='\0' */
   char    *ca;	                 /* consensus accessibility line  1..M    (p7H_CA)         */ /* String; 0=' ', M+1='\0' */
+
   char    *comlog;               /* command line(s) that built model      (optional: NULL) */ /* String, \0-terminated   */
   int      nseq;	         /* number of training sequences          (optional: -1)   */
   float    eff_nseq;             /* effective number of seqs (<= nseq)    (optional: -1)   */
@@ -392,6 +394,7 @@ enum p7_hmmfile_formats_e {
   p7_HMMFILE_3b = 2,
   p7_HMMFILE_3c = 3,
   p7_HMMFILE_3d = 4,
+  p7_HMMFILE_3e = 5,
 };
 
 typedef struct p7_hmmfile_s {
@@ -1076,7 +1079,6 @@ extern int     p7_hmm_CreateBody(P7_HMM *hmm, int M, const ESL_ALPHABET *abc);
 extern void    p7_hmm_Destroy(P7_HMM *hmm);
 extern int     p7_hmm_CopyParameters(const P7_HMM *src, P7_HMM *dest);
 extern P7_HMM *p7_hmm_Clone(const P7_HMM *hmm);
-extern int     p7_hmm_Scale(P7_HMM *hmm, double scale);
 extern int     p7_hmm_Zero(P7_HMM *hmm);
 extern char    p7_hmm_EncodeStatetype(char *typestring);
 extern char   *p7_hmm_DecodeStatetype(char st);
@@ -1087,8 +1089,9 @@ extern int     p7_hmm_SetDescription(P7_HMM *hmm, char *desc);
 extern int     p7_hmm_AppendComlog  (P7_HMM *hmm, int argc, char **argv);
 extern int     p7_hmm_SetCtime      (P7_HMM *hmm);
 extern int     p7_hmm_SetComposition(P7_HMM *hmm);
+extern int     p7_hmm_SetConsensus  (P7_HMM *hmm, ESL_SQ *sq);
 /*      3. Renormalization and rescaling counts in core HMMs. */
-extern int     p7_hmm_Rescale(P7_HMM *hmm, float scale);
+extern int     p7_hmm_Scale      (P7_HMM *hmm, double scale);
 extern int     p7_hmm_Renormalize(P7_HMM *hmm);
 /*      4. Debugging and development code. */
 extern int     p7_hmm_Dump(FILE *fp, P7_HMM *hmm);
@@ -1128,6 +1131,7 @@ extern int          p7_pipeline_Merge  (P7_PIPELINE *p1, P7_PIPELINE *p2);
 
 extern int p7_pli_TargetReportable  (P7_PIPELINE *pli, float score,     double lnP);
 extern int p7_pli_DomainReportable  (P7_PIPELINE *pli, float dom_score, double lnP);
+
 extern int p7_pli_TargetIncludable  (P7_PIPELINE *pli, float score,     double lnP);
 extern int p7_pli_DomainIncludable  (P7_PIPELINE *pli, float dom_score, double lnP);
 extern int p7_pli_NewModel          (P7_PIPELINE *pli, const P7_OPROFILE *om, P7_BG *bg);

src/itest_brute.c
@@ -343,6 +343,7 @@ create_brute_hmm(ESL_ALPHABET *abc, struct p7_bruteparam_s *prm)
   hmm->nseq     = 0;
   hmm->eff_nseq = 0;
   p7_hmm_SetCtime(hmm);
+  p7_hmm_SetConsensus(hmm, NULL);
   hmm->checksum = 0;
 
   return hmm;

commit d50484bb331e4af4f77690afb56e89953228b6c1

Fixed an hmmpgmd bug reported by RDF; a copy/paste typo in receiving new query HMM and calculating its length in memory; was doing strlen(hmm->name) three times instead of name, acc, desc.  Bug was introduced in dev trunk in r3569, 16 Jun 2011.
git-svn-id: https://svn.janelia.org/eddylab/eddys/src/hmmer/trunk@3756 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

src/hmmdwrkr.c
@@ -388,8 +388,8 @@ process_QueryCmd(HMMD_COMMAND *cmd, WORKER_ENV *env)
     memcpy(*hmm->ins, p, n);   p += n;
 
     if (thmm.name) { hmm->name = strdup(p); p += strlen(hmm->name) + 1; }
-    if (thmm.acc)  { hmm->acc  = strdup(p); p += strlen(hmm->name) + 1; }
-    if (thmm.desc) { hmm->desc = strdup(p); p += strlen(hmm->name) + 1; }
+    if (thmm.acc)  { hmm->acc  = strdup(p); p += strlen(hmm->acc)  + 1; }
+    if (thmm.desc) { hmm->desc = strdup(p); p += strlen(hmm->desc) + 1; }
 
     n = hmm->M + 2;
     if (hmm->flags & p7H_RF)   { memcpy(hmm->rf,        p, n); p += n; }

src/hmmpgmd.c
@@ -206,7 +206,7 @@ main(int argc, char **argv)
  * in three separate terminal windows    
  *     valgrind --dsymutil=yes --leak-check=yes --show-reachable=yes src/hmmpgmd --master --seqdb foo.d 
  *     valgrind                --leak-check=yes --show-reachable=yes src/hmmpgmd --worker 127.0.0.1 --cpu 1 
- *     valgrind                --leak-check=yes --show-reachable=yes src/hmmc2 -S
+ *     valgrind --dsymutil=yes --leak-check=yes --show-reachable=yes src/hmmc2 -S
  *     
  * copy/paste tutorial/M1.hmm as an example hmmsearch    
  * or any HMM. On Mac OS/X, to "copy" a file my.hmm to the clipboard: