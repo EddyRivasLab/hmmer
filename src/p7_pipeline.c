dde50c845c91abb8dc878ae8763005ee7ac412de
Travis Wheeler <travis.wheeler@traviswheeler.com>
2012-10-17 13:57:07 +0000

    p7_pipeline.c: In yesterday's commit, I moved the p7_oprofile_GetFwdEmissionArray() call into p7_Pipeline_LongTarget(), but mistakenly placed it before the nhmmscan call to p7_oprofile_ReadRest(). The result was that nhmmscan didn't have correct forward scores established at the time the function was called, leading to an underflow error. Moved call to after p7_oprofile_ReadRest().
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/hmmer/branches/nhmmer@4267 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- src/p7_pipeline.c
+++ src/p7_pipeline.c
@@ -1435,8 +1435,6 @@ p7_Pipeline_LongTarget(P7_PIPELINE *pli, P7_OPROFILE *om, P7_SCOREDATA *data, P7
   ESL_ALLOC(scores_arr, (om->abc->Kp)*4*sizeof(float));
   ESL_ALLOC(fwd_emissions_arr, sizeof(float) *  om->abc->Kp * (om->M+1));
 
-  p7_oprofile_GetFwdEmissionArray(om, bg, fwd_emissions_arr);
-
 
   msv_windowlist.windows = NULL;
   vit_windowlist.windows = NULL;
@@ -1469,6 +1467,8 @@ p7_Pipeline_LongTarget(P7_PIPELINE *pli, P7_OPROFILE *om, P7_SCOREDATA *data, P7
         if ((status = p7_pli_NewModelThresholds(pli, om)) != eslOK) goto ERROR;
       }
     }
+
+    p7_oprofile_GetFwdEmissionArray(om, bg, fwd_emissions_arr);
 
     if (data->prefix_lengths == NULL) { //otherwise, already filled in
       p7_hmm_ScoreDataComputeRest(om, data);
