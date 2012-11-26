f92348cc535cb63b915889636c853783e8b059cc
Eric Nawrocki <eric.nawrocki@gmail.com>
2012-11-26 15:09:13 +0000

    Manually merged changes from trunk revision r4311 to Infernal 1.1 release branch, to get hmmer's testsuite to run cleanly on FreeBSD.
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/hmmer/branches/infernal/1.1@4315 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- src/makenhmmerdb.c
+++ src/makenhmmerdb.c
@@ -452,9 +452,9 @@ main(int argc, char *argv[]) {
 
   output_header(stdout, go, fname_in, fname_out);
 
-  meta->SA_shift = log(meta->freq_SA) * eslCONST_LOG2R; /* i.e. log2() */
-  meta->cnt_shift_b = log(meta->freq_cnt_b) * eslCONST_LOG2R;
-  meta->cnt_shift_sb = log(meta->freq_cnt_sb) * eslCONST_LOG2R;
+  meta->SA_shift     = (int) round(log(meta->freq_SA)     * eslCONST_LOG2R); /* i.e. log2() */
+  meta->cnt_shift_b  = (int) round(log(meta->freq_cnt_b)  * eslCONST_LOG2R);
+  meta->cnt_shift_sb = (int) round(log(meta->freq_cnt_sb) * eslCONST_LOG2R);
 
   //getInverseAlphabet
   fm_createAlphabet(meta, &(meta->charBits));
