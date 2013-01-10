334f377a653c3b75208d9a907f71c3bb62f99a5d
Sean Eddy <sean@eddylab.org>
2013-01-10 18:30:45 +0000

    Nightly build tests updated to include MPI.
    Nightlies will build and test MPI implementation on two platforms: 
     intel-macosx-gcc-mpi        : Mac OS/X, GNU gcc,   OpenMPI 1.2.8  
     intel-linux-icc-intel64-mpi : Linux     Intel icc  Intel MPI 4.1
    
    Intel MPI only runs on cluster nodes, not on login-eddy head node.
    Updated hmmer_autobuilds.pl so it can use qsub, in a rube
    goldberg-esque kludge (nightly cron job runs hmmer_autobuild.pl on
    Mac, Mac ssh's to login-eddy, login-eddy submits a synced qsub to run
    autobuild.pl on a cluster node; then the stacked scripts/commands are
    carefully rigged so that output of autobuild.pl comes back to
    hmmer_autobuild.pl).
    
    Cluster nodes apparently can't build ia32 binaries. MPI build for ia32
    removed. Our icc ia32 build is now intel-linux-icc-ia32, on login-eddy
    head node, without MPI.
    
    Nightlies now test seven configurations.
     intel-macosx-gcc             OS/X    gcc  ./configure
     intel-macosx-gcc-debug       OS/X    gcc  ./configure --enable-debugging
     intel-macosx-gcc-mpi         OS/X    gcc  ./configure --enable-mpi
     intel-linux-gcc              Linux   gcc  ./configure
     intel-linux-icc-intel64-mpi  Linux   icc  ./configure --enable-mpi
     intel-linux-icc-ia32         Linux   icc  ./configure 
     intel-linux-gcc-ubuntu32     Ubuntu  gcc  ./configure
     intel-freebsd-gcc            FreeBSD gcc  ./configure
    
    where platforms are
     "wol", 10.102.30.160, OS/X 10.6.8; gcc 4.2.1; OpenMPI 1.2.8
     login-eddy, Scientific Linux SL release 5.3 (Boron); gcc 4.1.2; icc 13.0.1; IntelMPI 4.1
     cf-ubuntu32, Ubuntu 12.04.1 LTS, gcc 4.6.3
     cf-freebsd, FreeBSD 8.2, gcc 4.2.1
    
    
    
    
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/hmmer/trunk@4363 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- autobuild/hmmer_autobuilds.pl
+++ autobuild/hmmer_autobuilds.pl
@@ -6,19 +6,18 @@
 # Example:   hmmer_autobuilds.pl  ~/nightlies/hmmer/trunk > /tmp/hmmer_autobuilds.log
 
 @buildconfigs = (
-#   { name => "intel-linux-icc-intel64-mpi", host => "login-eddy"  },
-#   { name => "intel-linux-icc-ia32-mpi",    host => "login-eddy"  },
-    { name => "intel-linux-gcc",             host => "login-eddy"  },
-    { name => "intel-macosx-gcc",            host => "."           },
-    { name => "intel-macosx-gcc-debug",      host => "."           },
-#   { name => "intel-macosx-gcc-dummy",      host => "."           },
-#   { name => "ppc-aix-xlc",                 host => "cf-ppc2"     },
-    { name => "intel-linux-gcc-ubuntu32",    host => "cf-ubuntu32" },
-    { name => "intel-freebsd-gcc",           host => "cf-freebsd"  },
-#   { name => "ppc-macosx",                  host => "10.41.4.30"  },
+    { name => "intel-macosx-gcc",            host => ".",            use_qsub => 0  },
+    { name => "intel-macosx-gcc-debug",      host => ".",            use_qsub => 0  },
+    { name => "intel-macosx-gcc-mpi",        host => ".",            use_qsub => 0  },
+    { name => "intel-linux-gcc",             host => "login-eddy",   use_qsub => 0  },
+    { name => "intel-linux-icc-intel64-mpi", host => "login-eddy",   use_qsub => 1  }, # Actually compiles/tests on a cluster node.
+    { name => "intel-linux-icc-ia32",        host => "login-eddy",   use_qsub => 0  },
+    { name => "intel-linux-gcc-ubuntu32",    host => "cf-ubuntu32",  use_qsub => 0  },
+    { name => "intel-freebsd-gcc",           host => "cf-freebsd",   use_qsub => 0  },
     );
 
-$autoconf = "/opt/local/bin/autoconf";
+$autoconf = "/opt/local/bin/autoconf";                          # on the build master: wol
+$qsub     = ". /sge/8.0.1p4/default/common/settings.sh; qsub";  # on the SGE master:   login-eddy
 
 if ($#ARGV+1 != 1) { die "FAIL: incorrect number of command line arguments"; }
 $srcdir = shift;
@@ -26,10 +25,10 @@ if (! -d $srcdir) { die "FAIL: source working directory $srcdir not found"; }
 
 # First we update in the source working directory.
 #
-chdir $srcdir ||    die "FAIL: couldn't cd to $srcdir"; 
-system("svn update > autobuilds.log 2>&1");                 if ($?) { die "FAIL: svn update"; }
-system("$autoconf  > autobuilds.log 2>&1 ");                if ($?) { die "FAIL: H3 $autoconf"; }
-system("(cd lib/easel; $autoconf) > autobuilds.log 2>&1");  if ($?) { die "FAIL: esl $autoconf"; }
+chdir $srcdir || die "FAIL: couldn't cd to $srcdir"; 
+system("svn update                > autobuilds.log 2>&1");   if ($?) { die "FAIL: svn update"; }
+system("$autoconf                 >> autobuilds.log 2>&1 "); if ($?) { die "FAIL: H3 $autoconf"; }
+system("(cd lib/easel; $autoconf) >> autobuilds.log 2>&1");  if ($?) { die "FAIL: esl $autoconf"; }
 
 # Then we try to build on everything
 #
@@ -37,9 +36,30 @@ foreach $build (@buildconfigs)
 {
     $script = $build->{name};
     $cmd = "$srcdir/autobuild/autobuild.pl $srcdir $script";
+    if ($build->{use_qsub})    { $cmd = "($qsub -j y -o $srcdir/autobuilds.qsub -N autobuild -sync y -V -cwd -b y '$cmd' > /dev/null 2>&1; cat $srcdir/autobuilds.qsub)"; } # See [qsub].
     if ($build->{host} ne ".") { $cmd = "ssh $build->{host} \"$cmd\""; }
 
     printf ("%-30s %-20s ", $build->{name}, $build->{host});
     $output = `$cmd`;
     print $output;
+
+    # cleanup
+    if ($build->{use_qsub}) { unlink "$srcdir/autobuilds.qsub"; }
 }
+
+
+# [qsub] On using qsub. 
+#     Rube Goldberg-esque, sorry.
+#     We want to run tests under Intel MPI.
+#     The Intel MPI environment runs on our cluster nodes, but not on login-eddy.
+#     We don't want to directly ssh into a cluster node; we want to go through qsub.
+#     qsub -sync y flag makes qsub wait on the command it runs, and return the exit status of that command.
+#     The autobuild.pl script writes its "ok" or "FAIL" message to stdout, and hmmer_autobuilds.pl expects to see it.
+#     qsub -o autobuilds.qsub captures the autobuild.pl output and writes it to the file $srcdir/autobuilds.qsub
+#     qsub's own output (stdout and stderr) we don't want, so send to /dev/null
+#     When the qsub is done, we cat the autobuilds.qsub file, to get it back on stdout where hmmer_autobuilds expects it.
+#     
+#     ssh is a non-interactive shell. It does not run /etc/profile, so it doesn't get Goran's SGE config set up.
+#     So we make a more complicated $qsub definition, which first sources Goran's config file.
+#
+#     Ta-da, that explains the magic incantation: "($qsub ... `$cmd`; cat autobuilds.qsub)"
