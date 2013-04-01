41a026b637c050fc7ca39c02fa9d93fce1c1fe5f
Bill Arndt <larofeticus@gmail.com>
2013-04-01 16:59:08 +0000

    Adding translation functions to easel, new miniapp, stats unpacking function in hmmpgmd2msa.c
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/hmmer/branches/3.1@4417 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- src/hmmpgmd2msa.c
+++ src/hmmpgmd2msa.c
@@ -220,7 +220,9 @@ hmmpgmd2msa(void *data, P7_HMM *hmm, ESL_SQ *qsq, int *incl, int incl_size, int 
       p += sizeof(P7_ALIDISPLAY);
 
       ESL_ALLOC(ad2->mem, ad2->memsize);
+      
       memcpy(ad2->mem, p, ad->memsize);
+      
       p += ad2->memsize;
       p7_alidisplay_Deserialize(ad2);
     }
@@ -261,6 +263,225 @@ ERROR:
   return status;
 }
 
+/******************************************************************************
+ *# 1. The <hmmpgmd2stats> function
+ *****************************************************************************/
+
+
+
+/* Function:  hmmpgmd2stats()
+ * Synopsis:  Use a HMMPGMD-derived data stream to extract some simple
+ *            statistics regarding its alignment.
+ * Purpose:   Given a data stream from HMMPGMD of the form shown
+ *            here, produce a vector of floats:
+ *                 positions 0 to hmm->M-1 are:
+ *                   Fraction of alignments which cover model at that position
+ *                 positions hmm->M to hmm->M*2-1
+ *                   Fraction of alignments which cover model at that position (mod hmm->M)     
+ *                   with a similar residue
+ *                 positions hmm->M*2 to hmm->M*3-1
+ *                   Fraction of alignments which cover model at that position (mod hmm->M)
+ *                   with the consensus residue
+ *
+ * Args :     data: a pointer to binary data in the format given above
+ *            hmm:  the HMM against which the alidisplay traces and
+ *                  additional sequences/traces are threaded to reach
+ *                  the returned msa.
+ *            
+ * Returns:   the location where the output vector will be placed.
+ *                      caller is responsible for freeing it later   
+ *
+ */
+int hmmpgmd2stats(void *data, P7_HMM *hmm, float** statsOut) 
+{
+  int i, j, k;
+  int status;
+
+  /* trace of the query sequence with N residues onto model with N match states */
+  P7_TRACE          *qtr         = NULL;
+
+  /* vars used to read from the binary data */
+  HMMD_SEARCH_STATS *stats   = NULL;              /* pointer to a single stats object, at the beginning of data */
+  P7_HIT            *hits    = NULL;              /* an array of hits, at the appropriate offset in data */
+
+  P7_TOPHITS         th;
+  P7_ALIDISPLAY     *ad, *ad2;
+
+  float *cover, *id, *similar; //store statistics results
+  int readPos, writePos;     //for converting alignment contents into model indexing
+
+  char              *p     = (char*)data;        /*pointer used to walk along data, must be char* to allow pointer arithmetic */
+
+  th.N = 0;
+  th.unsrt = NULL;
+  th.hit   = NULL;
+
+  //storage for coverage, %id and %similar accumulations
+  ESL_ALLOC( cover,   sizeof(float) * hmm->M * 3);
+  similar = cover+hmm->M+1;
+  id = similar+hmm->M+1;
+  //ESL_ALLOC( id,      sizeof(int) * hmm->M);
+  //ESL_ALLOC( similar, sizeof(int) * hmm->M);
+  for(k = 0; k < hmm->M; k++)
+  {
+    cover[k] = 0;
+    id[k] = 0;
+    similar[k] = 0;
+  }
+
+  /* get search stats + hit info */
+  stats = (HMMD_SEARCH_STATS*)p;
+
+  /* sanity check */
+  if (   ( stats->Z_setby != p7_ZSETBY_NTARGETS    && stats->Z_setby != p7_ZSETBY_OPTION    && stats->Z_setby != p7_ZSETBY_FILEINFO )
+      || ( stats->domZ_setby != p7_ZSETBY_NTARGETS && stats->domZ_setby != p7_ZSETBY_OPTION && stats->domZ_setby != p7_ZSETBY_FILEINFO )
+      ||   stats->nhits > 10000000
+      ||   stats->elapsed > 1000000
+  ) {
+    status = eslFAIL;
+    goto ERROR;
+  }
+
+  /* ok, it looks legitimate */
+  p    += sizeof(HMMD_SEARCH_STATS);
+  hits  = (P7_HIT*)p;
+  p    += sizeof(P7_HIT) * stats->nhits;
+
+  /* create a tophits object, use it to step through the alignments */
+  ESL_ALLOC( th.unsrt, sizeof(P7_HIT) * stats->nhits);
+  memcpy( th.unsrt, hits, sizeof(P7_HIT) * stats->nhits);
+  ESL_ALLOC( th.hit, sizeof(P7_HIT*) * stats->nhits);
+  for (i=0; i<stats->nhits; i++) {
+    th.hit[i] = &(th.unsrt[i]);
+    if (   th.hit[i]->ndom > 10000
+        || th.hit[i]->flags >  p7_IS_INCLUDED + p7_IS_REPORTED + p7_IS_NEW + p7_IS_DROPPED + p7_IS_DUPLICATE
+    ) {
+      status = eslFAIL;
+      goto ERROR;
+    }
+  }
+
+  th.N         = stats->nhits;
+  th.nreported = 0;
+  th.nincluded = 0;
+  th.is_sorted_by_sortkey = 0;
+  th.is_sorted_by_seqidx  = 0;
+
+  for (i = 0; i < th.N; i++) {
+    ESL_ALLOC( th.hit[i]->dcl, sizeof(P7_DOMAIN) *  th.hit[i]->ndom);
+   /* first grab all the P7_DOMAINs for the hit */
+    for (j=0; j < th.hit[i]->ndom; j++) 
+    {
+      th.hit[i]->dcl[j].is_included = 1;
+      p += sizeof(P7_DOMAIN);
+    }
+    /* then grab the P7_ALIDISPLAYs for the hit */
+    for (j=0; j < th.hit[i]->ndom; j++) {
+      ad = (P7_ALIDISPLAY*)p;
+      ESL_ALLOC(th.hit[i]->dcl[j].ad, sizeof(P7_ALIDISPLAY));
+      ad2 = th.hit[i]->dcl[j].ad;
+
+      ad2->memsize = ad->memsize;
+      ad2->rfline = ad->rfline;
+      ad2->mmline = ad->mmline;
+      
+      ad2->csline = ad->csline ;
+      ad2->model  = ad->model ;
+      ad2->mline  = ad->mline ;
+      ad2->aseq   = ad->aseq ;
+      ad2->ppline = ad->ppline;
+      ad2->N      = ad->N;
+
+      ad2->hmmname = ad->hmmname;
+      ad2->hmmacc  = ad->hmmacc ;
+      ad2->hmmdesc = ad->hmmdesc;
+      ad2->hmmfrom = ad->hmmfrom;
+      ad2->hmmto   = ad->hmmto;
+      ad2->M       = ad->M;
+
+      ad2->sqname  = ad->sqname;
+      ad2->sqacc   = ad->sqacc ;
+      ad2->sqdesc  = ad->sqdesc;
+      ad2->sqfrom  = ad->sqfrom;
+      ad2->sqto    = ad->sqto;
+      ad2->L       = ad->L;
+       
+      p += sizeof(P7_ALIDISPLAY);
+
+      ESL_ALLOC(ad2->mem, ad2->memsize);
+      
+      memcpy(ad2->mem, p, ad->memsize);
+      
+      p += ad2->memsize;
+      p7_alidisplay_Deserialize(ad2);
+      
+      readPos = 0;
+      writePos = ad2->hmmfrom; //skip part of the hmm which isn't covered
+     
+      while(readPos < ad2->hmmto)
+      {
+        //check if model covers residue
+        if(isupper(ad2->aseq[readPos]) || ad2->aseq[readPos] == '-')
+        {
+          cover[writePos]++;
+        
+          //check mline for id
+          if(isalpha(ad2->mline[readPos]))
+          {
+            id[writePos]++;
+            similar[writePos]++;
+          }
+          //check mline for not-a-space
+          else if(ad2->mline[readPos] == '+')
+          {
+            similar[writePos]++;
+          }
+          writePos++;
+        }
+        readPos++;
+      } 
+    }
+  }
+
+  for(i = 0; i < hmm->M * 3; i++)
+    cover[i] = cover[i]/stats->nhits;
+  
+  *statsOut = cover;
+
+  /* free memory */
+  if (qtr != NULL) free(qtr);
+  for (i = 0; i < th.N; i++) {
+    for (j=0; j < th.hit[i]->ndom; j++)
+      p7_alidisplay_Destroy(th.hit[i]->dcl[j].ad);
+
+    if (th.hit[i]->dcl != NULL) free (th.hit[i]->dcl);
+  }
+  if (th.unsrt != NULL) free (th.unsrt);
+  if (th.hit != NULL) free (th.hit);
+
+  return eslOK;
+
+ERROR:
+  /* free memory */
+  if(cover != NULL) free(cover);
+  if(id != NULL) free(id);
+  if(similar != NULL) free(similar);
+  
+  if (qtr != NULL) free(qtr);
+
+  for (i = 0; i < th.N; i++) {
+    for (j=0; j < th.hit[i]->ndom; j++)
+      p7_alidisplay_Destroy(th.hit[i]->dcl[j].ad);
+
+    if (th.hit[i]->dcl != NULL) free (th.hit[i]->dcl);
+  }
+  if (th.unsrt != NULL) free (th.unsrt);
+  if (th.hit != NULL) free (th.hit);
+
+  *statsOut = NULL;
+
+  return status;
+}
 
 
 
@@ -269,7 +490,7 @@ ERROR:
  * 2. Test driver
  *****************************************************************/
 
-#define hmmpgmd2msa_TESTDRIVE
+//#define hmmpgmd2msa_TESTDRIVE
 #ifdef hmmpgmd2msa_TESTDRIVE
 
 /* Test driver. As written, requires files that won't be released with
@@ -289,13 +510,14 @@ main(int argc, char **argv) {
 
   char   errbuf[eslERRBUFSIZE];
   int status;
+  
+  float* statsOut;
+  int x;
   //char *badstring = "asdlfuhasdfuhasdfhasdfhaslidhflaishdfliasuhdfliasuhdfliasudfh";
 
   char *hmm_file = "esl_align.hmm";
   char *fa_file  = "esl_align.fa";
   char *dat_file = "esl_align.big.bin";
-
-
 
   if (argc > 1 ) {
     ESL_ALLOC(hmm_file, sizeof(char) * (strlen(argv[1])+1) );
@@ -322,7 +544,7 @@ main(int argc, char **argv) {
   qsq = esl_sq_CreateDigital(abc);
   if ( (status = esl_sqio_Read(qfp, qsq)) != eslOK)  goto ERROR;
 
-
+  //printf("sequence length %d\n", qsq->n);
 
   /* get stats for the hmmd data */
 
@@ -334,7 +556,25 @@ main(int argc, char **argv) {
   ESL_ALLOC(data, size);
   fread(data, size, 1, fp);
 
+  status = hmmpgmd2stats(data, hmm, &statsOut);
   status = hmmpgmd2msa(data, hmm, qsq, NULL,0, NULL, 0, &msa);
+
+
+  for(x = 0; x < hmm->M; x++)
+  {
+    printf("%1d", (int)(10*statsOut[x]));
+  }
+  printf("\n");
+  for(x = hmm->M; x < hmm->M*2; x++)
+  {
+    printf("%1d", (int)(10*statsOut[x]));
+  }
+  printf("\n");
+  for(x = hmm->M*2; x < hmm->M*3; x++)
+  {
+    printf("%1d", (int)(10*statsOut[x]));
+  }
+  printf("\n");
 
   if (status != eslOK) goto ERROR;
 
