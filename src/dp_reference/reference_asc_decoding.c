a8197aef1b847519bf9c90b6acb5dbff7e25016a
Sean Eddy <sean@eddylab.org>
2014-04-23 20:11:09 +0000

    Starting to add unit tests to ASC decoding, and starting to debug it... moving over to laptop for the moment.
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/hmmer/trunk@4654 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- src/dp_reference/reference_asc_decoding.c
+++ src/dp_reference/reference_asc_decoding.c
@@ -7,8 +7,9 @@
  * 
  * Contents:
  *    1. ASC Decoding
- *    x. Example
- *    x. Copyright and license information
+ *    2. Unit tests
+ *    3. Test driver
+ *    5. Copyright and license information
  */
 
 #include "p7_config.h"
@@ -149,9 +150,8 @@ p7_ReferenceASCDecoding(const ESL_DSQ *dsq, int L, const P7_PROFILE *gm, const P
 
   for (d = 0; d < D; d++)
     {
-      /* UP matrix 
-       */
-      iend = (d == 0 ? 1 : anch[d-1].n1 + 1);
+      /* UP matrix */
+      iend = (d == 0 ? 1 : anch[d-1].n1+1);
       for (i = iend; i < anch[d].n1; i++)
 	{
 	  /* Wing retraction of G->D1..Dk-1->MGk paths.
@@ -182,7 +182,7 @@ p7_ReferenceASCDecoding(const ESL_DSQ *dsq, int L, const P7_PROFILE *gm, const P
 	  fwdp  = afu->dp[i] + p7R_NSCELLS;
 	  bckp  = abu->dp[i] + p7R_NSCELLS;
 	  ppp   = apu->dp[i];
-	  denom = *ppp;		/* pick up what we stashed earlier; we're now going to finish row i and renormalize if needed */
+	  denom = *ppp;	 /* pick up what we stashed earlier; we're now going to finish row i and renormalize if needed */
 	  for (s = 0; s < p7R_NSCELLS; s++) *ppp++ = 0.0;
 
 	  /* Main decoding recursion:
@@ -202,17 +202,35 @@ p7_ReferenceASCDecoding(const ESL_DSQ *dsq, int L, const P7_PROFILE *gm, const P
 	      ppp  += p7R_NSCELLS;
 	    }
 
-#ifndef p7REFERENCE_ASC_DECODING_TESTDRIVE
-	  denom = 1.0 / denom;		    // multiplication may be faster than division
+	  //#ifndef p7REFERENCE_ASC_DECODING_TESTDRIVE
+	  denom = 1.0 / denom;		                                             // multiplication may be faster than division
 	  ppp   = apu->dp[i] + p7R_NSCELLS;                                          // that's k=1 in UP
 	  for (s = 0; s < (anch[d].n2-1) * p7R_NSCELLS; s++) *ppp++ *= denom;        // UP matrix row i renormalized
 	  ppp   = apd->dp[i] + (anch[d].n2) * p7R_NSCELLS;                           // that's k0 in DOWN
 	  for (s = 0; s < (M - anch[d].n2 + 1) * p7R_NSCELLS; s++) *ppp++ *= denom;  // DOWN matrix row i renormalized
 	  for (s = 0; s < p7R_NXCELLS; s++) ppp[s] *= denom;
-#endif
+	  //#endif
 	} /* end loop over i's in UP chunk for domain d */
 
-
+      /* One last wing-retracted row:
+       * On last row of each UP chunk (anch[d].i-1 row), those DG's
+       * are reachable on a G(i)->D1...Dk-1->MG(i+1,k) path that we
+       * need to unfold; and that MG(i+1,k) is the anchor itself,
+       * whose value is in the DOWN matrix.
+       */
+      i     = anch[d].n1;	
+      k     = anch[d].n2;
+      xG    = *(afu->dp[i-1] + (M+1) * p7R_NSCELLS + p7R_G); // ugly, sorry. Reach out and get the xG value on row i.
+      bckp  = abd->dp[i]      + k * p7R_NSCELLS;             // *bckp is the anchor cell i0,k0 (in DOWN)
+      rsc   = gm->rsc[dsq[i]] + k * p7P_NR;
+      delta = expf(xG + TSC(p7P_GM, k-1) + *rsc + bckp[p7R_MG] - totsc);  // k-1 in TSC because GMk is stored off by one, in -1 slot
+      ppp   = apu->dp[i-1] + p7R_NSCELLS;                    // ppp starts on i-1,k=1 cells
+      for (k = 1; k < anch[d].n2; k++) 
+	{
+	  ppp[p7R_DG] += delta;   // unlike the main wing retraction above, there's only one path going through these 
+	  ppp += p7R_NSCELLS;     //   DGk's (the G->Mk0 entry into the anchor cell), so there's no retro-accumulation of delta
+	}
+      
 
       /* DOWN matrix */
       xJ = xC = -eslINFINITY;
@@ -232,6 +250,10 @@ p7_ReferenceASCDecoding(const ESL_DSQ *dsq, int L, const P7_PROFILE *gm, const P
 	      ppp[p7R_IG] = expf(fwdp[p7R_IG] + bckp[p7R_IG] - totsc); denom += ppp[p7R_IG];
 	      ppp[p7R_DL] = expf(fwdp[p7R_DL] + bckp[p7R_DL] - totsc);
 	      ppp[p7R_DG] = expf(fwdp[p7R_DG] + bckp[p7R_DG] - totsc);
+
+	      fwdp += p7R_NSCELLS;
+	      bckp += p7R_NSCELLS;
+	      ppp  += p7R_NSCELLS;
 	    }
 	  /* fwdp, bckp, ppp now all sit at M+1, start of specials */
 
@@ -247,7 +269,7 @@ p7_ReferenceASCDecoding(const ESL_DSQ *dsq, int L, const P7_PROFILE *gm, const P
 
 	  
 	  if (d < D-1) *(apu->dp[i]) = denom;  // hack: stash denom, which we'll pick up again when we do the next UP matrix.
-#ifndef p7REFERENCE_ASC_DECODING_TESTDRIVE
+	  //#ifndef p7REFERENCE_ASC_DECODING_TESTDRIVE
 	  if (d == D-1) 
 	    { // UP matrices only go through anch[D-1].i-1, so for last DOWN chunk, we need to renormalize.
 	      denom = 1.0 / denom;
@@ -255,12 +277,199 @@ p7_ReferenceASCDecoding(const ESL_DSQ *dsq, int L, const P7_PROFILE *gm, const P
 	      for (s = 0; s < (M - anch[d].n2 + 1) * p7R_NSCELLS; s++) *ppp++ *= denom; 
 	      for (s = 0; s < p7R_NXCELLS; s++) ppp[s] *= denom;
 	    }
-#endif
+	  //#endif
 	} /* end loop over i's in DOWN chunk for domain d*/
 
     } /* end loop over domains d=0..D-1 */
   return eslOK;
 }
+/*------------------ end, ASC decoding --------------------------*/
+
+
+/*****************************************************************
+ * 2. Unit tests
+ *****************************************************************/
+#ifdef p7REFERENCE_ASC_DECODING_TESTDRIVE
+#include "hmmer.h"
+
+/* General note:
+ * The unit tests are essentially the same as the unit tests
+ * for Forward/Backward, but carried through to the additional
+ * step of posterior decoding. Single pathed tests verify 
+ * that the decoding matrix has pp=1.0 along the trace, 0.0
+ * elsewhere. Ensemble tests compare standard to ASC matrices.
+ */
+
+
+
+
+
+/* "singlepath" test.
+ * 
+ * p7_modelsample_SinglePathed() gives us a profile HMM contrived
+ * such that there is only one possible path; the profile must be
+ * uniglocal L=0 to do this, and thus lacks N/C/J emissions, local
+ * alignment, and multihit.
+ * 
+ * Since only one path is possible, every cell along this path decodes
+ * to pp=1.0, and all other cells to 0.0. 
+ * 
+ * We test:
+ *    1. All scores are equal (V, F, B, ASC F, ASC B)
+ *    2. Vit trace = generated trace
+ *    3. ASC Decoding matrix has 1.0 for all cells in trace
+ *    4. ASC Decoding matrix = standard Decoding mx, cell by cell.
+ */
+static void
+utest_singlepath(ESL_RANDOMNESS *rng, int M, const ESL_ALPHABET *abc)
+{
+  char        failmsg[] = "reference_asc_decoding singlepath unit test failed";
+  P7_BG      *bg        = p7_bg_Create(abc);
+  P7_HMM     *hmm       = NULL;
+  P7_PROFILE *gm        = p7_profile_Create(M, bg->abc);
+  ESL_SQ     *sq        = esl_sq_CreateDigital(bg->abc);
+  P7_TRACE   *tr        = p7_trace_Create();
+  P7_TRACE   *vtr       = p7_trace_Create();
+  P7_COORD2  *anch      = malloc(sizeof(P7_COORD2));   // utest is uniglocal, so it will have D=1 and only one anchor.
+  P7_REFMX   *rxv       = p7_refmx_Create(M, 20);      // 20 is arbitrary; all DP routines will reallocate as needed.
+  P7_REFMX   *rxf       = p7_refmx_Create(M, 20);
+  P7_REFMX   *rxb       = p7_refmx_Create(M, 20);
+  P7_REFMX   *rxd       = p7_refmx_Create(M, 20);
+  P7_REFMX   *afu       = p7_refmx_Create(M, 20);
+  P7_REFMX   *afd       = p7_refmx_Create(M, 20);
+  P7_REFMX   *abu       = p7_refmx_Create(M, 20);
+  P7_REFMX   *abd       = p7_refmx_Create(M, 20);
+  P7_REFMX   *apu       = p7_refmx_Create(M, 20);
+  P7_REFMX   *apd       = p7_refmx_Create(M, 20);
+  int         D;
+  int         nm, z, which;
+  float       sc, vsc, fsc, bsc, asc_f, asc_b;
+  float       epsilon   = ( p7_logsum_IsSlowExact() ? 0.0001 : 0.01 );
+  int         status;
+
+  /* Sample single-pathed HMM, create uniglocal L=0 profile */
+  if ((status = p7_modelsample_SinglePathed(rng, M, bg->abc, &hmm)) != eslOK) esl_fatal(failmsg);
+  if ( p7_profile_ConfigUniglocal(gm, hmm, bg, 0)                   != eslOK) esl_fatal(failmsg); 
+
+  /* Emit sequence and trace. Get the trace score. */
+  if (p7_ProfileEmit(rng, hmm, gm, bg, sq, tr) != eslOK) esl_fatal(failmsg);
+  if (p7_trace_Index(tr)                       != eslOK) esl_fatal(failmsg);
+  if (p7_trace_Score(tr, sq->dsq, gm, &sc)     != eslOK) esl_fatal(failmsg);
+
+  /* Create anchor "set". We know it's uniglocal, so D=1. 
+   * Choose anchor by randomly choosing a match state in the trace.
+   */
+  D  = 1;
+  for (nm = 0, z = 1; z < tr->N; z++) 
+    if (p7_trace_IsM(tr->st[z])) nm++;
+  which = 1 + esl_rnd_Roll(rng, nm);
+  for (z = 1; z < tr->N; z++) 
+    if (p7_trace_IsM(tr->st[z])) 
+      {
+	which--;
+	if (which == 0) {
+	  anch[0].n1 = tr->i[z];
+	  anch[0].n2 = tr->k[z];
+	  break;
+	}
+      }
+
+  /* Run the DP routines, both standard and ASC */
+  if ((status = p7_ReferenceViterbi (sq->dsq, sq->n, gm, rxv, vtr, &vsc)) != eslOK) esl_fatal(failmsg);
+  if ((status = p7_ReferenceForward (sq->dsq, sq->n, gm, rxf,      &fsc)) != eslOK) esl_fatal(failmsg);
+  if ((status = p7_ReferenceBackward(sq->dsq, sq->n, gm, rxb,      &bsc)) != eslOK) esl_fatal(failmsg);
+  if ((status = p7_ReferenceDecoding(sq->dsq, sq->n, gm, rxf, rxb,  rxd)) != eslOK) esl_fatal(failmsg);
+
+  if ((status = p7_ReferenceASCForward (sq->dsq, sq->n, gm, anch, D, afu, afd, &asc_f))             != eslOK) esl_fatal(failmsg);
+  if ((status = p7_ReferenceASCBackward(sq->dsq, sq->n, gm, anch, D, abu, abd, &asc_b))             != eslOK) esl_fatal(failmsg);
+  if ((status = p7_ReferenceASCDecoding(sq->dsq, sq->n, gm, anch, D, afu, afd, abu, abd, apu, apd)) != eslOK) esl_fatal(failmsg);
+
+  //printf("### Reference Vit:\n");      p7_refmx_Dump(stdout, rxv);
+  //printf("### Reference Fwd:\n");      p7_refmx_Dump(stdout, rxf);
+  //printf("### Reference Bck:\n");      p7_refmx_Dump(stdout, rxb);
+  printf("### Reference Decoding:\n"); p7_refmx_Dump(stdout, rxd);
+  //printf("### ASC Fwd UP:\n");         p7_refmx_Dump(stdout, afu);
+  //printf("### ASC Fwd DOWN:\n");       p7_refmx_Dump(stdout, afd);
+  //printf("### ASC Bck UP:\n");         p7_refmx_Dump(stdout, abu);
+  //printf("### ASC Bck DOWN:\n");       p7_refmx_Dump(stdout, abd);
+  printf("### ASC Decode UP:\n");      p7_refmx_Dump(stdout, apu);
+  printf("### ASC Decode DOWN:\n");    p7_refmx_Dump(stdout, apd);
+  //p7_trace_DumpAnnotated(stdout, tr,  gm, sq->dsq);
+  //p7_trace_DumpAnnotated(stdout, vtr, gm, sq->dsq);
+
+  p7_refmx_Destroy(apu);   p7_refmx_Destroy(apd);
+  p7_refmx_Destroy(abu);   p7_refmx_Destroy(abd);
+  p7_refmx_Destroy(afu);   p7_refmx_Destroy(afd);
+  p7_refmx_Destroy(rxv);
+  p7_refmx_Destroy(rxf);
+  p7_refmx_Destroy(rxb);
+  p7_refmx_Destroy(rxd);
+  p7_trace_Destroy(tr);
+  p7_trace_Destroy(vtr);
+  free(anch);
+  esl_sq_Destroy(sq);
+  p7_hmm_Destroy(hmm);
+  p7_profile_Destroy(gm);
+  p7_bg_Destroy(bg);
+}
+#endif /*p7REFERENCE_ASC_DECODING_TESTDRIVE*/
+/*-------------------- end, unit tests --------------------------*/
+
+
+
+/*****************************************************************
+ * 3. Test driver
+ *****************************************************************/
+#ifdef p7REFERENCE_ASC_DECODING_TESTDRIVE
+
+#include "p7_config.h"
+
+#include "easel.h"
+#include "esl_alphabet.h"
+#include "esl_getopts.h"
+#include "esl_random.h"
+
+#include "hmmer.h"
+
+static ESL_OPTIONS options[] = {
+  /* name           type      default  env  range toggles reqs incomp  help                                       docgroup*/
+  { "-h",        eslARG_NONE,   FALSE, NULL, NULL,  NULL,  NULL, NULL, "show brief help on version and usage",           0 },
+  { "-s",        eslARG_INT,      "0", NULL, NULL,  NULL,  NULL, NULL, "set random number seed to <n>",                  0 },
+  {  0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+static char usage[]  = "[-options]";
+static char banner[] = "unit test driver for reference ASC posterior decoding";
+
+int
+main(int argc, char **argv)
+{
+  ESL_GETOPTS    *go   = p7_CreateDefaultApp(options, 0, argc, argv, banner, usage);
+  ESL_RANDOMNESS *rng  = esl_randomness_Create(esl_opt_GetInteger(go, "-s"));
+  ESL_ALPHABET   *abc  = esl_alphabet_Create(eslAMINO);
+  int             M    = 10;
+
+  fprintf(stderr, "## %s\n", argv[0]);
+  fprintf(stderr, "#  rng seed = %" PRIu32 "\n", esl_randomness_GetSeed(rng));
+
+  utest_singlepath     (rng, M, abc);
+  //utest_singlesingle   (rng, M, abc);
+  //utest_singlemulti    (rng, M, abc);
+  //utest_multisingle    (rng, M, abc);
+  //utest_multipath_local(rng, M, abc);
+  //utest_multimulti     (rng, M, abc);
+
+  fprintf(stderr, "#  status = ok\n");
+
+  esl_alphabet_Destroy(abc);
+  esl_randomness_Destroy(rng);
+  esl_getopts_Destroy(go);
+  return 0;
+}
+#endif /*p7REFERENCE_ASC_DECODING_TESTDRIVE*/
+/*-------------------- end, test driver -------------------------*/
+
+
+
 
 
 /*****************************************************************
