87c92395bda2144214424eac77106ef13b802e5c
Sean Eddy <sean@eddylab.org>
2014-07-21 12:37:35 +0000

    Forgot to add sparse_anchors.h to that last commit.
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/hmmer/trunk@4704 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

---
+++ src/dp_sparse/sparse_anchors.h
@@ -0,0 +1,33 @@
+#ifndef p7SPARSE_ANCHORS_INCLUDED
+#define p7SPARSE_ANCHORS_INCLUDED
+
+#include "p7_config.h"
+
+#include "easel.h"
+#include "esl_random.h"
+
+#include "base/p7_anchors.h"
+#include "base/p7_profile.h"
+#include "base/p7_trace.h"
+
+#include "dp_sparse/p7_sparsemx.h"
+
+#include "search/p7_mpas.h"
+
+extern int p7_sparse_Anchors(ESL_RANDOMNESS *rng, const ESL_DSQ *dsq, int L, const P7_PROFILE *gm,
+			     float fwdsc, const P7_SPARSEMX *sxf, const P7_SPARSEMX *sxd,
+			     P7_TRACE *tr, float **byp_wrk, P7_ANCHORHASH *ah,
+			     P7_SPARSEMX *asf, P7_ANCHORS *anch, float *ret_asc,
+			     P7_MPAS_PARAMS *prm, P7_MPAS_STATS *stats);
+
+
+extern int p7_sparse_anchors_SetFromTrace(const P7_SPARSEMX *sxd, const P7_TRACE *tr, P7_ANCHORS *anch);
+
+
+#endif /*p7SPARSE_ANCHORS_INCLUDED*/
+/*****************************************************************
+ * @LICENSE@
+ * 
+ * SVN $Id$
+ * SVN $URL$
+ *****************************************************************/
