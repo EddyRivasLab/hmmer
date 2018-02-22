b383ab34c2334c2091376dc00dfc890e56088715
cryptogenomicon <sean@eddylab.org>
2018-02-22 09:31:42 -0500

    Improves quietness of `make`, especially `make clean`.
    Also adds -L flag to the find's in makeTAGS.sh, to follow symlink into Easel.

--- makeTAGS.sh
+++ makeTAGS.sh
@@ -5,14 +5,15 @@ etags -a INSTALL
 etags -a LICENSE
 
 # Recursively add all .c, .h, .pl, *.tex, *.man
-find . -name "*.c"   -print -or -name "*.h"  -print | xargs etags -a
-find . -name "*.pl"  -print -or -name "*.pm" -print | xargs etags -a
-find . -name "*.sh"  -print                         | xargs etags -a
-find . -name "*.tex" -print                         | xargs etags -a
-find . -name "*.man" -print                         | xargs etags -a
-find . -name "*.in"  -print                         | xargs etags -a
-find . -name "*.sqc" -print                         | xargs etags -a
-find . -name "*README"    -print                    | xargs etags -a
+#  -L : follow symlinks; Easel may be a symlink, for example
+find -L . -name "*.c"      -print -or -name "*.h"  -print | xargs etags -a
+find -L . -name "*.pl"     -print -or -name "*.pm" -print | xargs etags -a
+find -L . -name "*.sh"     -print                         | xargs etags -a
+find -L . -name "*.tex"    -print                         | xargs etags -a
+find -L . -name "*.man"    -print                         | xargs etags -a
+find -L . -name "*.in"     -print                         | xargs etags -a
+find -L . -name "*.sqc"    -print                         | xargs etags -a
+find -L . -name "*README*" -print                         | xargs etags -a
 
 etags -a documentation/man/boilerplate-tail
 
