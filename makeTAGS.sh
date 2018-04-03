7ba1e5975e4eafebbde1081e5974f116a55aa522
cryptogenomicon <sean@eddylab.org>
2018-04-03 14:34:06 -0400

    Overhauls how 'make install' works.
    now we install only (18) programs and their man pages, + hmmer.man
    page. We no longer install libhmmer.a, pdf, or .h headers.
    
    hmmerfm-exactmatch and hmmc2 removed from installed ${PROGS}, moved to
    an ${AUXPROGS} list that gets built but not installed.
    
    `make install` is now always verbose about what it's doing; removed
    quiet beautification.
    
    Variable substitution in .tex and .man files is now done by
    ./configure from *.in files, not by outmoded sedition script.
    
    Removed ifneq conditionals from Makefiles. We're aiming to have a
    github master branch clone act identically to a release tarball, so
    release procedure can have no file modifications or
    removals. Supersedes need to check for existence of documentation/,
    release-notes/.
    
    removed LICENSE.sh, LICENSE.tag, also as part of simplifying release
    procedure.
    
    updated HISTORY with 3.x release dates.
    
    updates config.sub, config.guess with new ones from gnu mothership.
    
    updates LICENSE with some additional credits.
    
    removes some empty or fragmented/unnecessary in-depths-of-time-anyway
    release notes.

--- makeTAGS.sh
+++ makeTAGS.sh
@@ -4,16 +4,18 @@ etags    configure.ac
 etags -a INSTALL
 etags -a LICENSE
 
-# Recursively add all .c, .h, .pl, *.tex, *.man
+# Recursively add all .c, .h, .pl, *.tex, *.in  (*.in includes Makefiles, .man's)
 #  -L : follow symlinks; Easel may be a symlink, for example
 find -L . -name "*.c"      -print -or -name "*.h"  -print | xargs etags -a
 find -L . -name "*.pl"     -print -or -name "*.pm" -print | xargs etags -a
 find -L . -name "*.sh"     -print                         | xargs etags -a
 find -L . -name "*.tex"    -print                         | xargs etags -a
-find -L . -name "*.man"    -print                         | xargs etags -a
 find -L . -name "*.in"     -print                         | xargs etags -a
 find -L . -name "*.sqc"    -print                         | xargs etags -a
 find -L . -name "*README*" -print                         | xargs etags -a
 
-etags -a documentation/man/boilerplate-tail
+# Easel
+etags -a easel/configure.ac
+etags -a easel/INSTALL
+etags -a easel/LICENSE
 
