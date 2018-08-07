979f19fe9bc0cdfc53d0d33d3cf60881dbe48855
Nick Carter <npcarter@g.harvard.edu>
2018-08-07 14:01:38 -0400

    Moving work over to Odyssey

---
+++ src/hmmpgmd_shard.h
@@ -0,0 +1,8 @@
+#ifndef P7_HMMPGMD_SHARD_INCLUDED
+#define P7_HMMPGMD_SHARD_INCLUDED
+
+extern void worker_process_shard(ESL_GETOPTS *go);
+extern void master_process_shard(ESL_GETOPTS *go);
+
+
+#endif /*P7_HMMPGMD_SHARD_INCLUDED*/
