34aad92c9636d64e379970bd6148a47e2cac3a9a
cryptogenomicon <sean@eddylab.org>
2020-01-11 11:25:36 -0500

    Fixes iss #176: p7_gmx_utest segfault for large L,M
    Pointer arithmetic in p7_gmx_Create(), p7_gmx_GrowTo() used integers
    that overflow for large L and M.
    
    Added (ptrdiff_t) casts. Added a test that catches the bug. Improved
    gmx_testpattern(), used by that test, to respect maximum representable
    integer in a float (see comment there), which also keeps runtime
    manageable when testing a very large (>2GB) matrix to try to exercise
    the bug.

--- src/p7_gmx.c
+++ src/p7_gmx.c
@@ -7,6 +7,7 @@
  *   4. Test driver
  */
 #include "p7_config.h"
+
 #include "hmmer.h"
 
 /*****************************************************************
@@ -46,7 +47,7 @@ p7_gmx_Create(int allocM, int allocL)
 
   /* Set the row pointers. */
   for (i = 0; i <= allocL; i++) 
-    gx->dp[i] = gx->dp_mem + i * (allocM+1) * p7G_NSCELLS;
+    gx->dp[i] = gx->dp_mem + (ptrdiff_t) i * (ptrdiff_t) (allocM+1) * (ptrdiff_t) p7G_NSCELLS;
 
   /* Initialize memory that's allocated but unused, only to keep
    * valgrind and friends happy.
@@ -135,7 +136,7 @@ p7_gmx_GrowTo(P7_GMX *gx, int M, int L)
       gx->allocW = M+1;
       gx->validR = ESL_MIN(gx->ncells / gx->allocW, gx->allocR);
       for (i = 0; i < gx->validR; i++) 
-	gx->dp[i] = gx->dp_mem + i * (gx->allocW) * p7G_NSCELLS;
+	gx->dp[i] = gx->dp_mem + (ptrdiff_t) i * (ptrdiff_t) (gx->allocW) * (ptrdiff_t) p7G_NSCELLS;
     }
 
   gx->M      = 0;
@@ -334,41 +335,56 @@ p7_gmx_DumpWindow(FILE *ofp, P7_GMX *gx, int istart, int iend, int kstart, int k
 #include "esl_random.h"
 #include "esl_randomseq.h"
 
+/* gmx_testpattern()
+ *
+ * Write a test pattern into the cells of <gx>, and read it back
+ * in different ways, to test that the memory is laid out as expected.
+ *
+ * <gmx> stores floats, and the test pattern depends on numbering each
+ * dp cell with an integer, so we must make sure our count doesn't
+ * exceed the largest exactly representable integer in a float:
+ * 2^24 = 16777216. Limiting the test pattern to 2^24 cells also helps
+ * keep runtime down.
+ */
 static void
 gmx_testpattern(P7_GMX *gx, int M, int L)
 {
-  int i,k,s,n, n2;
+  int64_t i;      // among other things, i counts thru gx->ncells, which is int64_t
+  int64_t n,n2;   // test pattern counts cells, so n and n2 also have to be int64_t
+  int     k,s;
+  int     maxL      = 16777216 / (3 * (M+1));        
+  int64_t start_row = (L > maxL ? L-maxL+1 : 0);
 
   /* Write a test pattern, via the dp[i] pointers */
   n = 0;
-  for (i = 0; i <= L; i++)
+  for (i = start_row; i <= L; i++)
     for (k = 0; k <= M; k++)
       for (s = 0; s < p7G_NSCELLS; s++)
-	gx->dp[i][k*p7G_NSCELLS+s] = n++;
+	gx->dp[i][k*p7G_NSCELLS+s] = (float) n++;
 
   /* Read it back, via the dp[i] pointers */
   n = 0;
-  for (i = 0; i <= L; i++)
+  for (i = start_row; i <= L; i++)
     for (k = 0; k <= M; k++)
       for (s = 0; s < p7G_NSCELLS; s++)
-		{
-		  if (gx->dp[i][k*p7G_NSCELLS+s] != n) esl_fatal("gmx unit test failed: test pattern corrupted");
-		  n++;
-		}
+	{
+	  if ((int) gx->dp[i][k*p7G_NSCELLS+s] != n) esl_fatal("gmx unit test failed: test pattern corrupted");
+	  n++;
+	}
   
   /* Reading it back via the dp_mem vector itself ought to be the same */
-  if (gx->allocR == gx->validR && gx->ncells == gx->validR*gx->allocW)
-  {
+  if (gx->allocR == gx->validR && gx->ncells == (int64_t) gx->validR * (int64_t) gx->allocW)
+    {
       n2 = 0;
-      for (i = 0; i < gx->ncells; i++)
-		for (s = 0; s < p7G_NSCELLS; s++)
-		  {
-			if (gx->dp_mem[i*p7G_NSCELLS+s] != n2) esl_fatal("gmx unit test failed: test pattern corrupted (2nd test)");
-			n2++;
-		  }
+      for (i = start_row * gx->allocW; i < gx->ncells; i++)
+	for (s = 0; s < p7G_NSCELLS; s++)
+	  {
+	    if (gx->dp_mem[i*p7G_NSCELLS+s] != n2) esl_fatal("gmx unit test failed: test pattern corrupted (2nd test)");
+	    n2++;
+	  }
       /* and the number of cells ought to match too */
       if (n != n2) esl_fatal("gmx unit test failed: unexpected # of cells");
-  }
+    }
 }
 
 
@@ -378,18 +394,21 @@ utest_GrowTo(void)
   int     M, L;
   P7_GMX *gx = NULL;
 
-  M = 20;  L = 20;  gx= p7_gmx_Create(M, L);  gmx_testpattern(gx, M, L);
-  M = 40;  L = 20;  p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);  /* grow in M, not L */
-  M = 40;  L = 40;  p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);  /* grow in L, not M */
-  M = 80;  L = 10;  p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);  /* grow in M, but with enough ncells */
-  M = 10;  L = 80;  p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);  /* grow in L, but with enough ncells */
-  M = 100; L = 100; p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);  /* grow in both L and M */
+  M = 20;    L = 20;    gx= p7_gmx_Create(M, L);  gmx_testpattern(gx, M, L);
+  M = 40;    L = 20;    p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);  /* grow in M, not L */
+  M = 40;    L = 40;    p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);  /* grow in L, not M */
+  M = 80;    L = 10;    p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);  /* grow in M, but with enough ncells */
+  M = 10;    L = 80;    p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);  /* grow in L, but with enough ncells */
+  M = 100;   L = 100;   p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);  /* grow in both L and M */
 
  /* The next two calls are carefully constructed to exercise bug #h79. 
   * GrowTo() must shrink allocW, if M shrinks and L grows enough to force increase in allocR, with sufficient ncells.
   */
-  M = 179; L = 55;  p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);
-  M = 87;  L = 57;  p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);
+  M = 179;   L = 55;    p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);
+  M = 87;    L = 57;    p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);
+
+  /* and this exercises iss#176 */
+  M = 71582; L = 10000; p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);  
 
   p7_gmx_Destroy(gx);
 }
