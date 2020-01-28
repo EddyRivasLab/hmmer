82899fedb308fa6f8034ae9712d0c40c7193e4ca
cryptogenomicon <sean@eddylab.org>
2020-01-28 11:40:24 -0500

    Fix for 'make check' failing on hmmc2 on FreeBSD.
    hmmc2.c was checking errno from a readn() call using a hardcoded 104
    instead of the portable ECONNRESET. FreeBSD uses a different code for
    ECONNRESET. Thanks to Motomichi Matsuzaki (RIKEN Advanced Intelligence
    Project) for the fix.

--- src/hmmc2.c
+++ src/hmmc2.c
@@ -334,7 +334,7 @@ int main(int argc, char *argv[])
       n = sizeof(sstatus);
       total += n;
       if ((size = readn(sock, &sstatus, n)) == -1) {
-        if(errno == 104 || errno == 0){
+        if(errno == ECONNRESET || errno == 0){
           // connection was reset, usually because server exited
           fprintf(stderr, "Daemon exited, shutting down\n");
           exit(0);
