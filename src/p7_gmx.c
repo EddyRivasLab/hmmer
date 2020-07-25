e1938d17e5febea4ef850860ed1dafd7a64c6993
cryptogenomicon <sean@eddylab.org>
2020-07-25 13:48:29 -0400

    Fixes p7_gmx_utest failure on 32-bit i686 system.
    p7_gmx_utest failed on gcc45 compile farm machine. Traced to integer
    overflow, requiring further revision to previous fixes for iss #176
    (see commits 0533666, 108aeb4).

--- src/p7_gmx.c
+++ src/p7_gmx.c
@@ -398,8 +398,9 @@ gmx_testpattern(P7_GMX *gx, int M, int L)
 static void
 utest_GrowTo(void)
 {
-  int     M, L;
   P7_GMX *gx = NULL;
+  int     M, L;
+  int64_t nbytes;
 
   M = 20;    L = 20;    gx= p7_gmx_Create(M, L);  gmx_testpattern(gx, M, L);
   M = 40;    L = 20;    p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);  /* grow in M, not L */
@@ -415,13 +416,15 @@ utest_GrowTo(void)
   M = 87;    L = 57;    p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);
 
   /* and this exercises iss#176. Only do this on 64-bit systems, and only if a large alloc is possible (we need 8.6G to exercise the bug!) */
-  if ( (uint64_t) (M+1) * (uint64_t) (L+1) * p7G_NSCELLS * sizeof(float) < SIZE_MAX / 2)
+  M = 71582; L = 10000;
+  nbytes = (int64_t) (M+1) * (int64_t) (L+1) * (int64_t) p7G_NSCELLS * (int64_t) sizeof(float);
+  if ( nbytes < SIZE_MAX / 2)
     {
-      void *p = malloc(10000000000ULL);  // check that a 10G allocation succeeds
+      void *p = malloc(nbytes);  // check that the allocation succeeds at all                                              
       if (p != NULL)
 	{
 	  free(p);
-	  M = 71582; L = 10000; p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);
+	  p7_gmx_GrowTo(gx, M, L);  gmx_testpattern(gx, M, L);
 	}
     }
 
