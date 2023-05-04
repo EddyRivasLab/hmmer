3348ea49d3dcb0df3b9d22a38653ba58bdf71549
Travis Wheeler <twheeler@arizona.edu>
2023-05-04 16:18:17 -0700

    Add flag for hmmsearcht to utils and daemon, to avoid crash

--- src/hmmdutils.c
+++ src/hmmdutils.c
@@ -93,6 +93,7 @@ static ESL_OPTIONS searchOpts[] = {
   { "-Z",           eslARG_REAL,      FALSE, NULL, "x>0",     NULL,  NULL, NULL,        "set # of comparisons done, for E-value calculation",          12 },
   { "--domZ",       eslARG_REAL,      FALSE, NULL, "x>0",     NULL,  NULL, NULL,        "set # of significant seqs, for domain E-value calculation",   12 },
   { "--hmmdb",      eslARG_INT,       NULL,  NULL, "n>0",   NULL,  NULL,  "--seqdb",       "hmm database to search",                                      12 },
+  { "--hmmscant",   eslARG_NONE,      NULL,  NULL, NULL,    NULL,  NULL,  "--seqdb",       "search hmm database with a 6 frame translated DNA sequence", 12 },
   { "--seqdb",      eslARG_INT,         NULL,  NULL, "n>0",   NULL,  NULL,  "--hmmdb",       "protein database to search",                                  12 },
   { "--seqdb_ranges",eslARG_STRING,     NULL,  NULL,  NULL,   NULL, "--seqdb", NULL,         "range(s) of sequences within --seqdb that will be searched",  12 },
   
